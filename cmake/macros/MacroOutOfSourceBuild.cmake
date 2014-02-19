8c04f22eaa575be107af7f3eedef4eadfed95d2b
pauletti <spauletti@gmail.com>
2014-02-19 19:30:39 -0300

    cleaning cmake macro code

--- cmake/macros/MacroOutOfSourceBuild.cmake
+++ cmake/macros/MacroOutOfSourceBuild.cmake
@@ -17,30 +17,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #-+--------------------------------------------------------------------
-#-------------------------------------------------------------------------------
-# Igatools a general purpose Isogeometric analysis library.
-# Copyright (C) 2012,2013  by the igatools authors (see authors.txt).
-# 
-# Distribution of this software is not allowed.
-# Only personal use by authorized people in the environment of
-# the IMATI institute is allowed.
-#   
-# When the software is reasonably stable it will be released under an 
-# Open Source license (GPLv3) as follows:
-# 
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-# 
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-# 
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#-------------------------------------------------------------------------------
 
 # Ensures that we do an out of source build
 macro( MACRO_ENSURE_OUT_OF_SOURCE_BUILD )
