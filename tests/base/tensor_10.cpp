065ae372ef3606eedaf1d8fa7e662e886b2aef69
pauletti <spauletti@gmail.com>
2014-02-21 15:47:14 -0300

    apply copyright header to newly created files

--- tests/base/tensor_10.cpp
+++ tests/base/tensor_10.cpp
@@ -17,6 +17,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //-+--------------------------------------------------------------------
+
 /*
  *  Test for redesign tensor class.
  *  Here test for the basic features like constructor and index access.
