0b4a61066231760e388174b5cd80d6dfd1108c09
Massimiliano Martinelli <massimiliano.martinelli@gmail.com>
2014-03-30 13:57:00 +0200

    Implemented the function BSplineElementScalarEvaluator::evaluate_derivative_at_points() (Not tested).

--- include/igatools/basis_functions/bspline_element_scalar_evaluator.h
+++ include/igatools/basis_functions/bspline_element_scalar_evaluator.h
@@ -75,8 +75,16 @@ private:
 
 
 /**
- * @brief blabla
- * @todo Document this class
+ * @brief Evaluator of scalar BSpline values and derivatives from one-dimensional values.
+ *
+ * This purpose of this class is to isolate the methods for computing the BSpline values and derivatives
+ * from the (precomputed) one-dimensional values (and derivative).
+ * Each instance of this class represents a single BSpline \f$ N_i : \mathbb{R}^{dim} \to \mathbb{R}\f$ that
+ * must be evaluated at some points arranged in a tensor-product way.
+ *
+ * @todo Document more
+ * @author M. Martinelli
+ * @date 29 Mar 2014
  */
 template <int dim>
 class
@@ -96,13 +104,23 @@ public:
 
     /** @name Constructors */
     ///@{
+    /** Default constructor. Not allowed to be used. */
     BSplineElementScalarEvaluator() = delete;
 
+    /**
+     * Constructor.
+     * It builds the scalar BSpline evaluator from the one-dimensional views of values and derivatives.
+     * @p values1D[i][j] are the <tt>i</tt>-th order (one-dimensional) derivatives along the <tt>j</tt>-th direction.
+     */
     BSplineElementScalarEvaluator(const std::vector<std::array<Values1DConstView,dim>> &values1D);
 
+    /** Copy constructor. */
     BSplineElementScalarEvaluator(const BSplineElementScalarEvaluator<dim> &bspline) = default;
+
+    /** Move constructor. */
     BSplineElementScalarEvaluator(BSplineElementScalarEvaluator<dim> &&bspline) = default;
 
+    /** Destructor. */
     ~BSplineElementScalarEvaluator() = default;
     ///@}
 
@@ -110,30 +128,55 @@ public:
 
     /** @name Assignment operators */
     ///@{
+    /** Copy assignment operator. */
     BSplineElementScalarEvaluator<dim> &operator=(const BSplineElementScalarEvaluator<dim> &bspline) = default;
+
+    /** Move assignment operator. */
     BSplineElementScalarEvaluator<dim> &operator=(BSplineElementScalarEvaluator<dim> &&bspline) = default;
     ///@}
 
 
+    /**
+     * Evaluate and returns one partial derivative in one point.
+     * The order of the partial derivative is specified by the tensor-index @p order_tensor_id,
+     * while the point is specified by its tensor-index @p point_tensor_id.
+     */
     Real evaluate_derivative(
         const TensorIndex<dim> &order_tensor_id,
         const TensorIndex<dim> &point_tensor_id) const;
 
 
+    /**
+     * Evaluate and returns one partial @p derivative in all points.
+     * The order of the partial derivative is specified by the tensor-index @p order_tensor_id.
+     */
     void evaluate_derivative_at_points(
         const TensorIndex<dim> &order_tensor_id,
-        DynamicMultiArray<Real,dim> & derivatives) const;
+        DynamicMultiArray<Real,dim> & derivative) const;
 
 
     /** Returns the number of points in each direction for which the 1D values are associated. */
     TensorSize<dim> get_num_points() const;
 
+    /**
+     * Returns a const view to the one-dimensional derivatives of given @p order,
+     * along all coordinate directions.
+     */
     const std::array<Values1DConstView,dim>& get_derivative_components_view(const int order) const;
 
+    /**
+     * Returns a const view to the one-dimensional derivative of given @p order,
+     * along the coordinate direction @p dir.
+     */
     const Values1DConstView & get_values_view(const int order,const int dir) const;
 
 private:
-
+/*
+    template <int k>
+    void recursive_multiplication(
+    	const TensorIndex<dim> &order_tensor_id,
+        DynamicMultiArray<Real,dim> & derivative) const;
+//*/
     /**
      * values[i][j] are the values at the n_qp evaluation points of the i-th derivative
      * along the j-th direction.
--- source/basis_functions/bspline_element_scalar_evaluator.cpp
+++ source/basis_functions/bspline_element_scalar_evaluator.cpp
@@ -95,7 +95,59 @@ get_num_points() const
 	}
 	return n_points;
 }
+/*
+template <int dim>
+template <int k>
+void
+BSplineElementScalarEvaluator<dim>::
+recursive_multiplication(
+	const TensorIndex<dim> &order_tensor_id,
+    DynamicMultiArray<Real,dim-k+1> & derivative) const
+{
+	Assert(false,ExcNotImplemented());
+	AssertThrow(false,ExcNotImplemented());
 
+	const int old_rank = dim-k+1;
+	const int new_dir = k-1;
+
+	TensorSize<rank> num_pts;
+	TensorSize<rank+1> num_pts_new;
+	for ( int rank = 0 ; rank < old_rank ; ++rank)
+	{
+		dir = dim-rank-1;
+		num_pts(rank) = this->get_num_points()(dir);
+		num_pts_new(rank+1) = num_pts(rank);
+
+		Assert(num_pts(rank) == derivative.tensor_size()(dir),
+				ExcDimensionMismatch(num_pts(rank),derivative.tensor_size()(dir)));
+	}
+	const Size pts_flat_size = num_pts.flat_size();
+
+	TensorIndex<rank> pt_tensor_id;
+	TensorIndex<rank> pt_tensor_w = MultiArrayUtils<rank>::compute_weight(num_pts);
+
+	//number of points in the new direction
+	const Size n_pts_new_dir = this->get_num_points()(new_dir);
+	num_pts_new(0) = n_pts_new_dir;
+
+	const auto &deriv_new_dir = values1D_[order_tensor_id[new_dir]][new_dir];
+	Index pt_flat_id_new = 0;
+	for (Index pt_flat_id = 0 ; pt_flat_id < pts_flat_size ; ++pt_flat_id)
+	{
+		//loop over the previous points
+		const Real value = derivative(pt_flat_id);
+
+		for (Index i = 0 ; i < n_pts_new_dir ; ++i,++pt_flat_id_new)
+		{
+			//loop over the new point index
+			dervative_new(pt_flat_id_new) = value * deriv_new_dir(i);
+		}
+
+	}
+
+	recursive_multiplication<k+1>(order_tensor_id,derivative_next);
+}
+//*/
 
 template <int dim>
 Real
@@ -109,8 +161,6 @@ evaluate_derivative(
         Assert(order_tensor_id[i] >= 0 && order_tensor_id[i] < values1D_.size(),
                ExcIndexRange(order_tensor_id[i],0,values1D_.size()));
 #endif
-
-    Assert(dim > 0, ExcLowerRange(dim,1));
 
     // Main computation
     Real partial_derivative = values1D_[order_tensor_id[0]][0](point_tensor_id[0]);
@@ -133,6 +183,88 @@ evaluate_derivative(
     return 0.0;
 }
 
+
+template <>
+void
+BSplineElementScalarEvaluator<1>::
+evaluate_derivative_at_points(
+    const TensorIndex<1> &order_tensor_id,
+    DynamicMultiArray<Real,1> & derivatives) const
+{
+	TensorSize<1> n_points = this->get_num_points();
+
+	Assert(derivatives.tensor_size()(0) == n_points(0),
+			ExcDimensionMismatch(derivatives.tensor_size()(0),n_points(0)));
+
+	const auto & deriv_dir_0 = this->get_derivative_components_view(order_tensor_id[0])[0];
+	for (Index flat_pt_id_0 = 0 ; flat_pt_id_0 < n_points(0) ; ++flat_pt_id_0)
+		derivatives(flat_pt_id_0) = deriv_dir_0(flat_pt_id_0);
+}
+
+template <>
+void
+BSplineElementScalarEvaluator<2>::
+evaluate_derivative_at_points(
+    const TensorIndex<2> &order_tensor_id,
+    DynamicMultiArray<Real,2> & derivatives) const
+{
+	TensorSize<2> n_points = this->get_num_points();
+
+#ifndef NDEBUG
+	for ( int dir = 0 ; dir < 2 ; ++dir)
+		Assert(derivatives.tensor_size()(dir) == n_points(dir),
+				ExcDimensionMismatch(derivatives.tensor_size()(dir),n_points(dir)));
+#endif
+
+	const auto & deriv_dir_1 = this->get_derivative_components_view(order_tensor_id[1])[1];
+	const auto & deriv_dir_0 = this->get_derivative_components_view(order_tensor_id[0])[0];
+
+	Index flat_pt_id = 0;
+	for (Index flat_pt_id_1 = 0 ; flat_pt_id_1 < n_points(1) ; ++flat_pt_id_1)
+	{
+		const Real deriv_dir_1_pt = deriv_dir_1(flat_pt_id_1);
+
+		for (Index flat_pt_id_0 = 0 ; flat_pt_id_0 < n_points(0) ; ++flat_pt_id_0)
+			derivatives(flat_pt_id++) = deriv_dir_0(flat_pt_id_0) * deriv_dir_1_pt;
+	}
+}
+
+template <>
+void
+BSplineElementScalarEvaluator<3>::
+evaluate_derivative_at_points(
+    const TensorIndex<3> &order_tensor_id,
+    DynamicMultiArray<Real,3> & derivatives) const
+{
+	TensorSize<3> n_points = this->get_num_points();
+
+#ifndef NDEBUG
+	for ( int dir = 0 ; dir < 3 ; ++dir)
+		Assert(derivatives.tensor_size()(dir) == n_points(dir),
+				ExcDimensionMismatch(derivatives.tensor_size()(dir),n_points(dir)));
+#endif
+
+	const auto & deriv_dir_2 = this->get_derivative_components_view(order_tensor_id[2])[2];
+	const auto & deriv_dir_1 = this->get_derivative_components_view(order_tensor_id[1])[1];
+	const auto & deriv_dir_0 = this->get_derivative_components_view(order_tensor_id[0])[0];
+
+	Index flat_pt_id = 0;
+	for (Index flat_pt_id_2 = 0 ; flat_pt_id_2 < n_points(2) ; ++flat_pt_id_2)
+	{
+		const Real deriv_dir_2_pt = deriv_dir_2(flat_pt_id_2);
+
+		for (Index flat_pt_id_1 = 0 ; flat_pt_id_1 < n_points(1) ; ++flat_pt_id_1)
+		{
+			const Real deriv_dir_1_pt =  deriv_dir_1(flat_pt_id_1);
+			const Real deriv_old_dirs_pt = deriv_dir_2_pt * deriv_dir_1_pt;
+
+			for (Index flat_pt_id_0 = 0 ; flat_pt_id_0 < n_points(0) ; ++flat_pt_id_0)
+				derivatives(flat_pt_id++) = deriv_dir_0(flat_pt_id_0) * deriv_old_dirs_pt;
+		}
+	}
+}
+
+
 template <int dim>
 void
 BSplineElementScalarEvaluator<dim>::
@@ -140,9 +272,6 @@ evaluate_derivative_at_points(
     const TensorIndex<dim> &order_tensor_id,
     DynamicMultiArray<Real,dim> & derivatives) const
 {
-	TensorSize<dim> n_points = this->get_num_points();
-
-
 	Assert(false,ExcNotImplemented());
 	AssertThrow(false,ExcNotImplemented());
 }
