5c80481079d3a45797aec9d0227f194f86382d63
Massimiliano Martinelli <massimiliano.martinelli@gmail.com>
2014-04-03 01:31:28 +0200

    Reordering the loops for the mass matrix with sum factorisation in the symmetric case

--- tests/basis_functions/example_sum_factorization.cpp
+++ tests/basis_functions/example_sum_factorization.cpp
@@ -625,17 +625,17 @@ public:
         } // end if(!is_symmetric)
         else
         {
+        	/*
+            LogStream out;
 
-            tensor_id_C_k[2] = 0;
             for (Index flat_beta_k_1 = 0 ; flat_beta_k_1 < size_flat_beta_k_1 ; ++flat_beta_k_1)
             {
                 tensor_id_C_k_1[2] = flat_beta_k_1;
 
-
                 for (int beta_k = 0 ; beta_k < tensor_size_beta[k-1] ; ++beta_k)
                 {
                     tensor_id_J_k[2] = beta_k;
-                    tensor_id_C_k[1] = 0 ;
+                    tensor_id_C_k[2] = flat_beta_k_1*tensor_size_beta[k-1] + beta_k ;
 
                     for (Index flat_alpha_k_1 = 0 ; flat_alpha_k_1 < size_flat_alpha_k_1 ; ++flat_alpha_k_1)
                     {
@@ -644,8 +644,8 @@ public:
                         for (int alpha_k = 0 ; alpha_k < tensor_size_alpha[k-1] ; ++alpha_k)
                         {
                             tensor_id_J_k[1] = alpha_k;
+                            tensor_id_C_k[1] = flat_alpha_k_1*tensor_size_alpha[k-1] + alpha_k;
 
-                            tensor_id_C_k_1[0] = 0 ;
                             for (Index flat_theta_k_1 = 0 ; flat_theta_k_1 < size_flat_theta_k_1 ; ++flat_theta_k_1)
                             {
                                 tensor_id_C_k[0] = flat_theta_k_1;
@@ -653,29 +653,90 @@ public:
                                 Real sum = 0.0;
                                 for (int theta_k = 0 ; theta_k < tensor_size_theta[k-1] ; ++theta_k)
                                 {
-                                    tensor_id_J_k[0] = theta_k;
+                                    tensor_id_C_k_1[0] = flat_theta_k_1*tensor_size_theta[k-1] + theta_k ;
+                                    tensor_id_J_k  [0] = theta_k;
+
                                     sum += C_k_1(tensor_id_C_k_1) * J_k(tensor_id_J_k);
-
-                                    tensor_id_C_k_1[0]++;
-
                                 } // end loop theta_k
 
                                 C_k(tensor_id_C_k) = sum;
+                                out << "tensor_id_C_k=" <<tensor_id_C_k <<std::endl;
 
                             } //end loop flat_theta_k_1
-
-//                    tensor_id_C_k[1]++;
-                            tensor_id_C_k[1] = flat_alpha_k_1*tensor_size_alpha[k-1] + alpha_k + 1;
 
                         } // end loop alpha_k
                     } // end loop flat_alpha_k_1
 
-                    tensor_id_C_k[2]++;
                 } // end loop beta_k
-
             } // end loop flat_beta_k_1
 
+//*/
 
+        	using MAUtils_k = MultiArrayUtils<k>;
+        	using MAUtils_k_1 = MultiArrayUtils<k-1>;
+
+        	const Size flat_size_alpha_k = tensor_size_alpha_k.flat_size();
+        	const TensorIndex<k> wgt_alpha_k = MAUtils_k::compute_weight(tensor_size_alpha_k);
+        	const TensorIndex<k-1> wgt_alpha_k_1 = MAUtils_k_1::compute_weight(tensor_size_alpha_k_1);
+
+        	const Size flat_size_beta_k = tensor_size_beta_k.flat_size();
+        	const TensorIndex<k> wgt_beta_k = MAUtils_k::compute_weight(tensor_size_beta_k);
+        	const TensorIndex<k-1> wgt_beta_k_1 = MAUtils_k_1::compute_weight(tensor_size_beta_k_1);
+
+        	TensorIndex<k-1> tensor_id_alpha_k_1;
+        	TensorIndex<k-1> tensor_id_beta_k_1;
+        	for (Index flat_id_beta_k = 0 ; flat_id_beta_k < flat_size_beta_k ; ++flat_id_beta_k)
+        	{
+        		const TensorIndex<k> tensor_id_beta_k = MAUtils_k::flat_to_tensor_index(flat_id_beta_k,wgt_alpha_k);
+        		for (int i = 0 ; i < k-1 ;++i)
+        			tensor_id_beta_k_1(i) = tensor_id_beta_k(i);
+
+        		const Index beta_k = tensor_id_beta_k(k-1);
+
+        		const Index flat_id_beta_k_1 = (k>1)?MAUtils_k_1::tensor_to_flat_index(tensor_id_beta_k_1,wgt_beta_k_1):0;
+
+    			tensor_id_C_k_1[2] = flat_id_beta_k_1;
+				tensor_id_C_k[2] = flat_id_beta_k ;
+				tensor_id_J_k[2] = beta_k;
+
+        		for (Index flat_id_alpha_k = 0 ; flat_id_alpha_k < flat_size_alpha_k ; ++flat_id_alpha_k)
+        		{
+        			const TensorIndex<k> tensor_id_alpha_k = MAUtils_k::flat_to_tensor_index(flat_id_alpha_k,wgt_beta_k);
+        			for (int i = 0 ; i < k-1 ;++i)
+        				tensor_id_alpha_k_1(i) = tensor_id_alpha_k(i);
+
+        			const Index alpha_k = tensor_id_alpha_k(k-1);
+
+        			const Index flat_id_alpha_k_1 = (k>1)?MAUtils_k_1::tensor_to_flat_index(tensor_id_alpha_k_1,wgt_alpha_k_1):0;
+
+                    tensor_id_C_k_1[1] = flat_id_alpha_k_1;
+                    tensor_id_C_k[1] = flat_id_alpha_k;
+                    tensor_id_J_k[1] = alpha_k;
+
+
+//                    tensor_id_C_k_1[0] = 0;
+                    for (Index flat_theta_k_1 = 0 ; flat_theta_k_1 < size_flat_theta_k_1 ; ++flat_theta_k_1)
+                    {
+                        tensor_id_C_k[0] = flat_theta_k_1;
+
+                        Real sum = 0.0;
+                        for (int theta_k = 0 ; theta_k < tensor_size_theta[k-1] ; ++theta_k)
+                        {
+                             tensor_id_C_k_1[0] = flat_theta_k_1*tensor_size_theta[k-1] + theta_k ;
+                             tensor_id_J_k  [0] = theta_k;
+
+                             sum += C_k_1(tensor_id_C_k_1) * J_k(tensor_id_J_k);
+//                             ++tensor_id_C_k_1[0];
+                        } // end loop theta_k
+
+                        C_k(tensor_id_C_k) = sum;
+//                                    out << "tensor_id_C_k=" <<tensor_id_C_k <<std::endl;
+
+                    } //end loop flat_theta_k_1
+
+        		}// end loop flat_id_alpha_k
+
+        	} // end loop flat_id_beta_k
         } // end if (symmetric)
 
 
@@ -1010,7 +1071,7 @@ void local_mass_matrix_from_phys_elem_accessor(
 
 
     // here we project the determinant Jacobian on a Bernstein polynomials space
-    auto K = perform_element_l2_projection_tp_basis<dim>(
+    const auto K = perform_element_l2_projection_tp_basis<dim>(
                  w_basis_proj_1D,
                  invM_projection,
                  quad_projection,
@@ -1045,7 +1106,13 @@ void local_mass_matrix_from_phys_elem_accessor(
     tensor_size_C_0[0] = n_basis_projection.flat_size(); // theta size
     tensor_size_C_0[1] = 1; // alpha size
     tensor_size_C_0[2] = 1; // beta size
-    K.reshape(tensor_size_C_0);
+
+    DynamicMultiArray<Real,3> C_0(tensor_size_C_0);
+    const Size n_entries = tensor_size_C_0.flat_size();
+
+    Assert(n_entries == K.flat_size(),ExcDimensionMismatch(n_entries,K.flat_size()));
+    for (int entry_id = 0 ; entry_id < n_entries ; ++entry_id)
+    	C_0(entry_id) = K(entry_id);
 
     const bool is_symmetric = true;
 
@@ -1055,7 +1122,8 @@ void local_mass_matrix_from_phys_elem_accessor(
                                          n_basis_trial,
                                          n_basis_test,
                                          moments,
-                                         K,is_symmetric);
+                                         C_0,
+                                         is_symmetric);
 
     Assert(C_ab.tensor_size()(1) == n_basis,
            ExcDimensionMismatch(C_ab.tensor_size()(1),n_basis));
@@ -1063,7 +1131,7 @@ void local_mass_matrix_from_phys_elem_accessor(
            ExcDimensionMismatch(C_ab.tensor_size()(2),n_basis));
 
 
-    if (is_symmetric)
+    if (true)
     {
         Index flat_id = 0 ;
         for (int test_id = 0 ; test_id < n_basis ; ++test_id)
@@ -1092,11 +1160,12 @@ void local_mass_matrix_from_phys_elem_accessor(
                 entry_tensor_id[0] = test_id;
                 entry_tensor_id[1] = trial_id;
 
-                const Index flat_id = MultiArrayUtils<2>::tensor_to_flat_index(entry_tensor_id,entry_wgt) ;
+//                const Index flat_id = MultiArrayUtils<2>::tensor_to_flat_index(entry_tensor_id,entry_wgt) ;
 
-                local_mass_matrix(test_id,trial_id) = C_ab(flat_id);
+//                local_mass_matrix(test_id,trial_id) = C_ab(flat_id);
+                local_mass_matrix(test_id,trial_id) = local_mass_matrix(trial_id,test_id);
             }
-
+//*/
     }
     // Assembly of the local mass matrix using sum-factorization -- end
     //----------------------------------------------------
@@ -1348,7 +1417,7 @@ assemble()
         this->rhs->add_block(loc_dofs, loc_rhs);
         //*/
 
-//        out<< "Local mass matrix sum-factorization=" << loc_mass_matrix_sf << endl << endl;
+        out<< "Local mass matrix sum-factorization=" << loc_mass_matrix_sf << endl << endl;
 //        out<< "Local mass matrix original=" << loc_mat << endl << endl;
         out<< "mass matrix difference=" << loc_mat - loc_mass_matrix_sf << endl << endl;
 
