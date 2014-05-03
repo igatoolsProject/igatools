4abf55fd54a543cdd9e21b2ee9e52d11026ca654
Massimiliano Martinelli <massimiliano.martinelli@gmail.com>
2014-05-03 13:02:20 +0200

    Added the empty interfaces of Matrix class for PETSc

--- include/igatools/linear_algebra/distributed_matrix.h
+++ include/igatools/linear_algebra/distributed_matrix.h
@@ -244,7 +244,7 @@ private:
     Teuchos::RCP<const Teuchos::Comm<int>> comm_ = Teuchos::createSerialComm<int>();
 
     void init(const SparsityPattern &sparsity_pattern);
-
+/*
     DeclException2(ExcInvalidIndex,
                    int, int,
                    << "The entry with index <" << arg1 << ',' << arg2
@@ -259,9 +259,237 @@ private:
                    << " are stored locally and can be accessed.");
 
     DeclException0(ExcNotQuadratic);
-
+//*/
 };
 #endif // #ifdef USE_TRILINOS
+
+
+
+
+
+
+
+#ifdef USE_PETSC
+/**
+ * @todo Missing documentation
+ *
+ *
+ */
+template <>
+class Matrix<LinearAlgebraPackage::petsc>
+{
+public:
+    /** Typedef for the matrix type */
+//    using WrappedMatrixType = Tpetra::CrsMatrix<Real,Index,Index> ;
+
+    using self_t = Matrix<LinearAlgebraPackage::petsc>;
+    using vector_t = Vector<LinearAlgebraPackage::petsc>;
+
+public:
+
+
+    /**@name Constructor and destructor */
+    ///@{
+    /** Default constructor */
+    Matrix() = delete;
+
+
+    /**
+     * Construct a distributed matrix with the dof distribution for its rows and column
+     * specified by the SparsityPattern @p sparsity_pattern.
+     */
+    Matrix(const SparsityPattern &sparsity_pattern);
+
+    /**
+     * Copy constructor. Not allowed to be used.
+     */
+    Matrix(const self_t &matrix) = delete;
+
+
+    /**
+     * Move constructor.
+     */
+    Matrix(self_t &&matrix) = default;
+
+
+
+    /** Destructor */
+    ~Matrix() = default;
+    ///@}
+
+    /**
+     * @name Function for creating Matrix objects
+     * @note These methods implement the "create idiom" and return
+     * a Matrix object wrapped by a std::shared_ptr
+     */
+    ///@{
+    /**
+     * Create a distributed matrix with the dof dostribution for its rows and column
+     * specified by the SparsityPattern @p sparsity_pattern.
+     */
+    static std::shared_ptr<self_t> create(const SparsityPattern &sparsity_pattern);
+    ///@}
+
+    /** @name Assignment operators */
+    ///@{
+
+    /**
+     * Copy assignment operator. Not allowed to be used.
+     */
+    Matrix &operator=(const self_t &matrix) = delete;
+
+    /**
+     * Move assignment operator. Not allowed to be used.
+     */
+    Matrix &operator=(self_t &&matrix) = delete;
+
+    ///@}
+
+
+
+    /** @name Methods for getting and/or modifying the matrix entries */
+    ///@{
+
+    /**
+     * Add the value @p input to the matrix entry (i,j).
+     * @note @p i and @p j are the global indices of the entry (i,j).
+     */
+    void add_entry(const Index i,const  Index j,const Real input);
+
+
+
+    /**
+     * \brief This function add the local matrix values to the global matrix, the local-to-global ids
+     * are passed as input argument.
+     * \param[in] row_glob_ids The vector containing the row global ids
+     * associated to the local matrix entries
+     * \param[in] col_glob_ids The vector containing the colume global ids
+     * associated to the local matrix entries
+     * \param[in] local_matrix The local matrix that must be added to the global matrix.
+     * \note The number of rows and columns of the local matrix must be equal to the dimension of the
+     * local_to_global vector, otherwise an exception will be raised.
+     */
+    void add_block(
+        const std::vector<Index> &row_glob_ids,
+        const std::vector<Index> &cln_glob_ids,
+        const DenseMatrix &local_matrix) ;
+
+    /**
+     * Given two global indices @p i and @p j, return the (i,j) entry of the matrix.
+     */
+    Real operator()(const Index i, const Index j) const;
+
+
+    /** Set the row specified by the global id @p row to zero. */
+    void clear_row(const Index row);
+    ///@}
+
+    /** @name Methods for retrieving the Trilinos objects wrapped by this class */
+    ///@{
+    /**
+     * Return the Trilinos RCP (Reference-Counted-Pointer, i.e. a smart pointer) wrapping the
+     * concrete Trilinos distributed matrix. Const version.
+     */
+//    Teuchos::RCP<const WrappedMatrixType> get_trilinos_matrix() const ;
+
+    /**
+     * Return the Trilinos RCP (Reference-Counted-Pointer, i.e. a smart pointer) wrapping the
+     * concrete Trilinos distributed matrix. Non-const version.
+     */
+//    Teuchos::RCP<WrappedMatrixType> get_trilinos_matrix() ;
+    ///@}
+
+
+    /** @name Methods for retrieving or printing the matrix informations */
+    ///@{
+    void print(LogStream &out) const ;
+
+
+    /** Return the number of global rows of the matrix */
+    Index get_num_rows() const ;
+
+
+    /** Return the number of global columns of the matrix */
+    Index get_num_columns() const ;
+    ///@}
+
+    /** @name Matrix-by-vector multiplication */
+    ///@{
+    /**
+     * Compute a sparse matrix-MultiVector multiply.
+     *
+     * This method computes y := beta * y + alpha * A * x.
+     * If beta == 0, this operation will enjoy overwrite semantics: y's entries will be ignored,
+     * and y will be overwritten with the result of the multiplication, even if it contains NaN
+     * (not-a-number) floating-point entries.
+     */
+    void multiply_by_right_vector(const vector_t &x,vector_t &y,const Real alpha,const Real beta) const;
+
+    /**
+     * Compute a sparse matrix-MultiVector multiply.
+     *
+     * This method computes y^T := beta * y^T + alpha * x^T * A.
+     * If beta == 0, this operation will enjoy overwrite semantics: y's entries will be ignored,
+     * and y will be overwritten with the result of the multiplication, even if it contains NaN
+     * (not-a-number) floating-point entries.
+     */
+    void multiply_by_left_vector(const vector_t &x,vector_t &y,const Real alpha,const Real beta) const;
+    ///@}
+
+
+    /** @name Methods for changing the filling state. */
+    ///@{
+    /**
+     * Signal that data entry is complete, specifying domain and range maps.
+     *
+     * Off-node indices are distributed, indices are sorted,
+     * redundant indices are eliminated, and global indices are transformed to local indices.
+     */
+    void fill_complete();
+
+    /**
+     * Resume operations that may change the values or structure of the matrix.
+     *
+     * This method must be called as a collective operation.
+     * Calling fill_complete() "freezes" both the values and the structure of the matrix.
+     * If you want to modify the matrix again, you must first call resume_fill().
+     * You then may not call resume_fill() again on that matrix until you first call fill_complete().
+     * You may make sequences of fill_complete(), resume_fill() calls as many times as you wish.
+     */
+    void resume_fill();
+    ///@}
+
+private:
+    /** The real Trilinos::TPetra matrix */
+//    Teuchos::RCP<WrappedMatrixType> matrix_ ;
+
+    /** Typedef for the dofs map across the processors */
+//    using dofs_map_t = Tpetra::Map<Index,Index>;
+
+//    Teuchos::RCP<dofs_map_t> row_space_map_;
+//    Teuchos::RCP<dofs_map_t> column_space_map_;
+
+//    Teuchos::RCP<const Teuchos::Comm<int>> comm_ = Teuchos::createSerialComm<int>();
+
+    void init(const SparsityPattern &sparsity_pattern);
+/*
+    DeclException2(ExcInvalidIndex,
+                   int, int,
+                   << "The entry with index <" << arg1 << ',' << arg2
+                   << "> does not exist.");
+
+    DeclException4(ExcAccessToNonLocalElement,
+                   int, int, int, int,
+                   << "You tried to access element (" << arg1
+                   << "/" << arg2 << ")"
+                   << " of a distributed matrix, but only rows "
+                   << arg3 << " through " << arg4
+                   << " are stored locally and can be accessed.");
+
+    DeclException0(ExcNotQuadratic);
+//*/
+};
+#endif // #ifdef USE_PETSC
 
 
 
--- source/linear_algebra/distributed_matrix.cpp
+++ source/linear_algebra/distributed_matrix.cpp
@@ -29,7 +29,23 @@ using std::shared_ptr;
 
 IGA_NAMESPACE_OPEN
 
+namespace
+{
+DeclException2(ExcInvalidIndex,
+               int, int,
+               << "The entry with index <" << arg1 << ',' << arg2
+               << "> does not exist.");
 
+DeclException4(ExcAccessToNonLocalElement,
+               int, int, int, int,
+               << "You tried to access element (" << arg1
+               << "/" << arg2 << ")"
+               << " of a distributed matrix, but only rows "
+               << arg3 << " through " << arg4
+               << " are stored locally and can be accessed.");
+
+DeclException0(ExcNotQuadratic);
+};
 
 #ifdef USE_TRILINOS
 
@@ -307,6 +323,343 @@ multiply_by_left_vector(const vector_t &x,vector_t &y,const Real alpha,const Rea
 }
 
 #endif //#ifdef USE_TRILINOS
+
+
+
+
+
+
+
+
+#ifdef USE_PETSC
+
+
+Matrix<LinearAlgebraPackage::petsc>::
+Matrix(const SparsityPattern &sparsity_pattern)
+{
+    init(sparsity_pattern) ;
+};
+
+
+void
+Matrix<LinearAlgebraPackage::petsc>::
+init(const SparsityPattern &sparsity_pattern)
+{
+	Assert(false,ExcNotImplemented());
+	AssertThrow(false,ExcNotImplemented());
+
+/*
+    //-------------------------------------------------------------------------------------
+    row_space_map_.reset(new dofs_map_t(sparsity_pattern.get_num_row_dofs(),
+                                        sparsity_pattern.get_row_dofs(),
+                                        0,comm_));
+
+    column_space_map_.reset(new dofs_map_t(sparsity_pattern.get_num_col_dofs(),
+                                           sparsity_pattern.get_col_dofs(),
+                                           0,comm_));
+    Teuchos::ArrayRCP<const long unsigned int> n_overlapping_funcs_per_row =
+        Teuchos::arcp(
+            Teuchos::RCP< const vector<long unsigned int> >(
+                new vector<long unsigned int>(sparsity_pattern.get_num_overlapping_funcs()))) ;
+
+    matrix_.reset(new WrappedMatrixType(row_space_map_,
+                                        column_space_map_,
+                                        n_overlapping_funcs_per_row));
+    //-------------------------------------------------------------------------------------
+
+
+
+    //-------------------------------------------------------------------------------------
+    // allocating the entries (to 0.0) in the matrix corresponding to the sparsitiy pattern
+    // (this step is required by the Tpetra matrix, in order to use sumIntoGlobalValues()
+    auto row     = sparsity_pattern.cbegin() ;
+    auto row_end = sparsity_pattern.cend() ;
+    for (; row != row_end ; ++row)
+    {
+        const Index row_id = row->first ;
+
+        const vector<Index> columns_id(row->second.begin(),row->second.end()) ;
+
+        const auto num_columns = columns_id.size() ;
+
+        vector<Real> values(num_columns, 0.0) ;
+
+        matrix_->insertGlobalValues(row_id, columns_id, values) ;
+    }
+    //-------------------------------------------------------------------------------------
+
+//*/
+};
+
+
+shared_ptr<Matrix<LinearAlgebraPackage::petsc> >
+Matrix<LinearAlgebraPackage::petsc>::
+create(const SparsityPattern &sparsity_pattern)
+{
+    return std::make_shared<self_t>(self_t(sparsity_pattern));
+}
+
+void
+Matrix<LinearAlgebraPackage::petsc>::
+add_entry(const Index row_id, const Index column_id, const Real value)
+{
+	Assert(false,ExcNotImplemented());
+	AssertThrow(false,ExcNotImplemented());
+/*
+    Teuchos::Array<Index> columns_id(1,column_id) ;
+    Teuchos::Array<Real> values(1,value) ;
+
+    matrix_->sumIntoGlobalValues(row_id,columns_id,values);
+   //*/
+};
+
+
+
+void
+Matrix<LinearAlgebraPackage::petsc>::
+add_block(
+    const vector<Index> &rows_id,
+    const vector<Index> &cols_id,
+    const DenseMatrix &local_matrix)
+{
+    Assert(!rows_id.empty(), ExcEmptyObject()) ;
+    Assert(!cols_id.empty(), ExcEmptyObject()) ;
+
+    const Index n_rows = rows_id.size();
+    const Index n_cols = cols_id.size();
+
+    Assert(n_rows == Index(local_matrix.size1()),
+           ExcDimensionMismatch(n_rows,Index(local_matrix.size1()))) ;
+    Assert(n_cols == Index(local_matrix.size2()),
+           ExcDimensionMismatch(n_cols,Index(local_matrix.size2()))) ;
+
+	Assert(false,ExcNotImplemented());
+	AssertThrow(false,ExcNotImplemented());
+/*
+    vector<Real> row_values(n_cols) ;
+    for (int i = 0 ; i < n_rows ; ++i)
+    {
+        const auto row_local_matrix = local_matrix.get_row(i) ;
+        for (int j = 0 ; j < n_cols ; ++j)
+            row_values[j] = row_local_matrix(j) ;
+
+        matrix_->sumIntoGlobalValues(rows_id[i],cols_id,row_values);
+    }
+//*/
+};
+
+
+
+void
+Matrix<LinearAlgebraPackage::petsc>::
+fill_complete()
+{
+	Assert(false,ExcNotImplemented());
+	AssertThrow(false,ExcNotImplemented());
+/*
+    matrix_->fillComplete();
+    //*/
+};
+
+void
+Matrix<LinearAlgebraPackage::petsc>::
+resume_fill()
+{
+	Assert(false,ExcNotImplemented());
+	AssertThrow(false,ExcNotImplemented());
+/*
+    matrix_->resumeFill();
+//*/
+};
+
+/*
+auto
+Matrix<LinearAlgebraPackage::petsc>::
+get_petsc_matrix() -> Teuchos::RCP<WrappedMatrixType>
+{
+    return matrix_;
+};
+
+auto
+Matrix<LinearAlgebraPackage::petsc>::
+get_petsc_matrix() const -> Teuchos::RCP<const WrappedMatrixType>
+{
+    return matrix_;
+};
+//*/
+
+Real
+Matrix<LinearAlgebraPackage::petsc>::
+operator()(const Index row, const Index col) const
+{
+	Assert(false,ExcNotImplemented());
+	AssertThrow(false,ExcNotImplemented());
+/*
+    const auto graph = matrix_->getGraph();
+
+    const auto row_map = graph->getRowMap();
+    const auto col_map = graph->getColMap();
+
+    const auto local_row = row_map->getLocalElement(row);
+    const auto local_col = col_map->getLocalElement(col);
+//    std::cout << "Global=("<<row<<","<<col<<")   Local=("<<local_row<<","<<local_col<<")"<<std::endl;
+
+    // If the data is not on the present processor, we throw an exception.
+    // This is one of the two tiny differences to the el(i,j) call,
+    //which does not throw any assertions, but returns 0.0.
+    Assert(local_row != Teuchos::OrdinalTraits<Index>::invalid(),
+           ExcAccessToNonLocalElement(row,col,
+                                      row_map->getMinGlobalIndex(),
+                                      row_map->getMaxGlobalIndex()+1));
+
+    Teuchos::ArrayView<const Index> local_col_ids ;
+    Teuchos::ArrayView<const Real> values ;
+    matrix_->getLocalRowView(local_row,local_col_ids,values) ;
+
+//    std::cout << local_col_ids <<std::endl ;
+//    std::cout << "local_col=" << local_col << std::endl;
+    // Search the index where we look for the value, and then finally get it.
+    const Index *col_find = std::find(local_col_ids.begin(),local_col_ids.end(),local_col);
+
+
+    // This is actually the only difference to the el(i,j) function,
+    // which means that we throw an exception in this case instead of just
+    // returning zero for an element that is not present in the sparsity pattern.
+    Assert(col_find != local_col_ids.end(), ExcInvalidIndex(row,col));
+
+    const Index id_find = static_cast<Index>(col_find-local_col_ids.begin());
+//    std::cout << "id_find="<<id_find<<std::endl;
+
+    return values[id_find];
+    //*/
+	return 0.0;
+}
+
+
+void
+Matrix<LinearAlgebraPackage::petsc>::
+clear_row(const Index row)
+{
+	Assert(false,ExcNotImplemented());
+	AssertThrow(false,ExcNotImplemented());
+/*
+	const auto graph = matrix_->getGraph();
+
+    const auto row_map = graph->getRowMap();
+    const auto local_row = row_map->getLocalElement(row);
+
+    // Only do this on the rows owned locally on this processor.
+    if (local_row != Teuchos::OrdinalTraits<Index>::invalid())
+    {
+        auto n_entries_row = graph->getNumEntriesInLocalRow(local_row);
+
+        vector<Index> local_col_ids(n_entries_row);
+
+        graph->getLocalRowCopy(local_row,local_col_ids,n_entries_row);
+        Assert(n_entries_row == graph->getNumEntriesInLocalRow(local_row),
+               ExcDimensionMismatch(n_entries_row,graph->getNumEntriesInLocalRow(local_row)));
+
+        vector<Real> zeros(n_entries_row,0.0);
+        matrix_->replaceLocalValues(local_row,local_col_ids,zeros);
+    }
+ //*/
+}
+
+
+
+void
+Matrix<LinearAlgebraPackage::petsc>::
+print(LogStream &out) const
+{
+	Assert(false,ExcNotImplemented());
+	AssertThrow(false,ExcNotImplemented());
+/*
+    using std::endl;
+
+    const auto n_global_rows = this->get_num_rows();
+
+    out << "-----------------------------" << endl;
+    // Commented as different petsc version show different information here
+    //   out << *matrix_ ;
+
+    out << "Row Index        Col Index        Value" << endl;
+    for (Index row_id = 0 ; row_id < n_global_rows ; ++row_id)
+    {
+        auto n_entries_row = matrix_->getNumEntriesInGlobalRow(row_id);
+
+        vector<Index> columns_id(n_entries_row);
+
+        vector<Real> values(n_entries_row) ;
+
+        matrix_->getGlobalRowCopy(row_id,columns_id,values,n_entries_row);
+
+        for (uint row_entry = 0 ; row_entry < n_entries_row ; ++row_entry)
+            out << row_id << "       " <<
+                columns_id[row_entry] << "        " <<
+                values[row_entry] << endl;
+    }
+    out << "-----------------------------" << endl;
+//*/
+}
+
+
+
+
+
+auto
+Matrix<LinearAlgebraPackage::petsc>::
+get_num_rows() const -> Index
+{
+	Assert(false,ExcNotImplemented());
+	AssertThrow(false,ExcNotImplemented());
+/*
+    return matrix_->getGlobalNumRows() ;
+    //*/
+	return 0;
+}
+
+auto
+Matrix<LinearAlgebraPackage::petsc>::
+get_num_columns() const -> Index
+{
+	Assert(false,ExcNotImplemented());
+	AssertThrow(false,ExcNotImplemented());
+	/*
+    return matrix_->getGlobalNumCols() ;
+    //*/
+	return 0;
+}
+
+
+void
+Matrix<LinearAlgebraPackage::petsc>::
+multiply_by_right_vector(const vector_t &x,vector_t &y,const Real alpha,const Real beta) const
+{
+	Assert(false,ExcNotImplemented());
+	AssertThrow(false,ExcNotImplemented());
+/*
+    matrix_->apply(*x.get_petsc_vector(),
+                   *y.get_petsc_vector(),
+                   Teuchos::NO_TRANS,alpha,beta);
+                   //*/
+}
+
+void
+Matrix<LinearAlgebraPackage::petsc>::
+multiply_by_left_vector(const vector_t &x,vector_t &y,const Real alpha,const Real beta) const
+{
+	Assert(false,ExcNotImplemented());
+	AssertThrow(false,ExcNotImplemented());
+/*
+    matrix_->apply(*x.get_petsc_vector(),
+                   *y.get_petsc_vector(),
+                   Teuchos::TRANS,alpha,beta);
+                   //*/
+}
+
+#endif //#ifdef USE_PETSC
+
+
 
 IGA_NAMESPACE_CLOSE
 
