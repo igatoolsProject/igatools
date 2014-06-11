73bbc5765fc8474b15ffbbc8dfc6fdaba19e1478
pauletti <spauletti@gmail.com>
2014-06-11 13:10:42 -0300

    updated and simplified nurbs iterator no cache tests

--- tests/basis_functions/nurbs_element_iterator_01_no_cache.cpp
+++ tests/basis_functions/nurbs_element_iterator_01_no_cache.cpp
@@ -24,7 +24,13 @@
 #include <igatools/basis_functions/nurbs_space.h>
 #include <igatools/basis_functions/nurbs_element_accessor.h>
 
-
+/*
+ *  Test for the NURBS space iterator using the no cache evaluations
+ *
+ *  author: pauletti
+ *  date: Jun 11, 2014
+ *
+ */
 
 template< int dim, int range, int rank = 1>
 void test()
@@ -58,58 +64,38 @@ void test()
     const auto points = quad.get_points().get_flat_cartesian_product();
 
 
-    auto element     = space->begin();
-    auto end_element = space->end();
+    auto elem     = space->begin();
+    auto end = space->end();
 
-    out.push("\t") ;
-    for (int j = 0 ; element != end_element ; ++j, ++element)
+    for (; elem != end; ++elem)
     {
-        out << "Element: " << j << endl ;
-
-        out.push("\t") ;
-
+        out << "Element: " << elem->get_flat_index()<< endl;
 
         out << "Values basis functions:" << endl ;
-        const auto values = element->evaluate_basis_values_at_points(points);
+        const auto values = elem->evaluate_basis_values_at_points(points);
         values.print_info(out) ;
-        out << endl ;
-
 
         out << "Gradients basis functions:" << endl ;
-        const auto gradients = element->evaluate_basis_gradients_at_points(points);
+        const auto gradients = elem->evaluate_basis_gradients_at_points(points);
         gradients.print_info(out) ;
-        out << endl ;
-
 
         out << "Hessians basis functions:" << endl ;
-        const auto hessians = element->evaluate_basis_hessians_at_points(points);
+        const auto hessians = elem->evaluate_basis_hessians_at_points(points);
         hessians.print_info(out) ;
-        out << endl ;
-
-
-        out.pop() ;
     }
-    out.pop() ;
-
-    out << endl ;
 }
 
 
-int main(int argc, char *argv[])
+int main()
 {
-    test< 1, 1 >() ;
+    test<1, 1>();
+    test<1, 2>();
+    test<1, 3>();
+    test<2, 1>();
+    test<2, 2>();
+    test<2, 3>();
+    test<3, 1>();
+    test<3, 3>();
 
-    test< 1, 2 >() ;
-
-    test< 1, 3 >() ;
-
-    test< 2, 1 >() ;
-
-    test< 2, 2 >() ;
-    test< 2, 3 >() ;
-    test< 3, 1 >() ;
-//  test< 3, 2 >() ;
-    test< 3, 3 >() ;
-//*/
-    return (0) ;
+    return 0;
 }
