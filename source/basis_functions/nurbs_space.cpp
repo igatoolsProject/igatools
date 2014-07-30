ef921b089af882982061119cf0d2dc6ccdcd1ef8
pauletti <spauletti@gmail.com>
2014-07-30 16:06:15 -0300

    tiny clean up

--- source/basis_functions/nurbs_space.cpp
+++ source/basis_functions/nurbs_space.cpp
@@ -427,17 +427,6 @@ print_info(LogStream &out) const
     for (auto w : weights_)
         w.print_info(out);
     out.end_item();
-
-//    out.push("\t");
-//    for (int comp_id = 0; comp_id < n_components; comp_id++)
-//    {
-//        const auto weights_component = weights_(comp_id).get_data();
-//        out << "weights[" << comp_id << "] = { ";
-//        for (const Real &w : weights_component)
-//            out << w << " ";
-//        out << "}" << endl;
-//    }
-//    out.pop();
 }
 
 
