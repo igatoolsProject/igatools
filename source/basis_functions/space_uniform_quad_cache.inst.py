d1413caf2930a51abb1dea32a58695a6de1ff2a8
pauletti <spauletti@gmail.com>
2014-10-08 16:39:33 -0300

    created physical space cache class

---
+++ source/basis_functions/space_uniform_quad_cache.inst.py
@@ -0,0 +1,36 @@
+#-+--------------------------------------------------------------------
+# Igatools a general purpose Isogeometric analysis library.
+# Copyright (C) 2012-2014  by the igatools authors (see authors.txt).
+#
+# This file is part of the igatools library.
+#
+# The igatools library is free software: you can use it, redistribute
+# it and/or modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation, either
+# version 3 of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#-+--------------------------------------------------------------------
+
+from init_instantiation_data import *
+
+include_files = ['basis_functions/bspline_element_accessor.h',
+                 'basis_functions/nurbs_element_accessor.h',
+                 'geometry/push_forward_element_accessor.h',
+                 'basis_functions/physical_space_element_accessor.h']
+
+data = Instantiation(include_files)
+(f, inst) = (data.file_output, data.inst)
+
+space_caches = ['SpaceUniformQuadCache<%s>' %x.name
+                for x in inst.PhysSpaces_v2]
+
+for row in space_caches:
+   f.write('template class %s; \n' % (row))
+   
