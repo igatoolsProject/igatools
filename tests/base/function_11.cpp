00fd0bd200d63a4e78ba14db3a2e93eddf816ec0
pauletti <spauletti@gmail.com>
2014-10-12 17:10:19 -0300

    added new test to develo IgFunction

--- tests/base/function_11.cpp
+++ tests/base/function_11.cpp
@@ -19,7 +19,7 @@
 //-+--------------------------------------------------------------------
 
 /*
- *  Test for Function class, as a prototype for an spline function
+ *  Test for Function class, as a prototype for an analytical function
  *  author: pauletti
  *  date: Oct 11, 2014
  */
@@ -28,18 +28,14 @@
 
 #include <igatools/base/quadrature_lib.h>
 #include <igatools/base/function_element.h>
-#include <igatools/basis_functions/bspline_space.h>
-#include <igatools/basis_functions/bspline_element_accessor.h>
-#include <igatools/linear_algebra/distributed_vector.h>
 
-template<class Space>
-class IgFunction : public NewFunction<Space::dim, Space::range, Space::rank>
+//#include <igatools/../../source/geometry/grid_forward_iterator.cpp>
+
+
+template<int dim, int range>
+class LinearFunction : public NewFunction<dim, range>
 {
 public:
-	static const int dim = Space::dim;
-	static const int range = Space::range;
-	static const int rank = Space::rank;
-
     using parent_t = NewFunction<dim, range>;
     using typename NewFunction<dim, range>::Point;
     using typename NewFunction<dim, range>::Value;
@@ -49,21 +45,16 @@ public:
     template <int order>
     using Derivative = typename parent_t::template Derivative<order>;
 
-    using CoeffType = Vector<LAPack::trilinos>;
-
-    IgFunction(const ValueFlags &flag, const Quadrature<dim> &quad,
-    		std::shared_ptr<const Space> space,
-    		const CoeffType &coeff)
-    :
-        parent_t::NewFunction(space->get_grid(), flag, quad),
+    LinearFunction(std::shared_ptr<const CartesianGrid<dim>> grid,
+                   const ValueFlags &flag, const Quadrature<dim> &quad,
+                   const Gradient &A, const Value &b)
+        :
+        parent_t::NewFunction(grid, flag, quad),
         flag_(flag),
         quad_(quad),
-        space_ (space),
-        coeff_(coeff),
-        elem_(space_->begin()),
-        space_filler_(space_, flag, quad)
+        A_ (A),
+        b_ (b)
     {}
-
 
     void init_element(ElementIterator &elem)
     {
@@ -76,34 +67,55 @@ public:
             cache = shared_ptr<Cache>(new Cache);
         }
         cache->resize(flag_, quad_.get_num_points());
-
-        space_filler_.init_element_cache(elem_);
     }
 
     void fill_element(ElementIterator &elem)
     {
         auto &el    = elem.get_accessor();
         GridUniformQuadCache<dim>::fill_element_cache(el);
-        space_filler_.fill_element_cache(elem_);
+        const auto points = el.get_points();
         auto &cache = this->get_cache(elem);
-
-        elem_.move_to(elem->get_flat_index());
-        const auto loc_coeff = coeff_.get_local_coefs(elem_->get_local_to_global());
         if (flag_.fill_values())
-        	cache->values_ = elem_->evaluate_field(loc_coeff);
+            evaluate_0(points, cache->values_);
         if (flag_.fill_gradients())
-        	std::get<1>(cache->derivatives_) = elem_->evaluate_field_gradients(loc_coeff);
+            evaluate_1(points, std::get<1>(cache->derivatives_));
         if (flag_.fill_hessians())
-        	std::get<2>(cache->derivatives_) = elem_->evaluate_field_hessians(loc_coeff);
+            evaluate_2(points, std::get<2>(cache->derivatives_));
+
+    }
+
+private:
+    void evaluate_0(const ValueVector<Point> &points,
+                    ValueVector<Value> &values) const
+    {
+        auto point = points.begin();
+        for (auto &val : values)
+        {
+            val = action(A_, *point) + b_;
+            ++point;
+        }
+    }
+
+
+    void evaluate_1(const ValueVector<Point> &points,
+                    ValueVector<Derivative<1>> &values) const
+    {
+        for (auto &val : values)
+            val = A_;
+    }
+
+    void evaluate_2(const ValueVector<Point> &points,
+                    ValueVector<Derivative<2>> &values) const
+    {
+        for (auto &val : values)
+            val = 0.;
     }
 
 private:
     ValueFlagsHandler flag_;
     Quadrature<dim> quad_;
-    std::shared_ptr<const Space> space_;
-    const CoeffType coeff_;
-    typename Space::ElementIterator elem_;
-    typename Space::UniformQuadCache space_filler_;
+    const Gradient A_;
+    const Value    b_;
 };
 
 
@@ -112,20 +124,24 @@ private:
 template<int dim, int range>
 void test()
 {
-	using Space = BSplineSpace<dim>;
-	using Function = IgFunction<Space>;
+    //const int n_points = 2;
+    using Function = LinearFunction<dim, range>;
 
+    typename Function::Value    b;
+    typename Function::Gradient A;
+    for (int i=0; i<range; i++)
+    {
+        for (int j=0; j<dim; j++)
+            if (j == i)
+                A[j][j] = 2.;
+        b[i] = i;
+    }
 
-    auto flag = ValueFlags::value| ValueFlags::gradient |  ValueFlags::hessian;
-//    auto flag = ValueFlags::point | ValueFlags::value | ValueFlags::gradient |
-//                ValueFlags::hessian;
+    auto flag = ValueFlags::point | ValueFlags::value | ValueFlags::gradient |
+                ValueFlags::hessian;
     auto quad = QGauss<dim>(2);
     auto grid = CartesianGrid<dim>::create(3);
-    const int deg = 1;
-    auto space = Space::create(deg, grid);
-    typename Function::CoeffType coeff(space->get_num_basis());
-    coeff(0) = 1.;
-    Function F(flag, quad, space, coeff);
+    Function F(grid, flag, quad, A, b);
 
 
     GridForwardIterator<FunctionElement<dim,range,1>> elem(grid, 0);
@@ -135,8 +151,8 @@ void test()
     for (; elem != end; ++elem)
     {
         F.fill_element(elem);
-        //elem->get_points().print_info(out);
-        //out << endl;
+        elem->get_points().print_info(out);
+        out << endl;
         elem->get_values().print_info(out);
         out << endl;
         elem->get_gradients().print_info(out);
@@ -149,7 +165,7 @@ void test()
 
 int main()
 {
-    test<2,1>();
+    test<2,2>();
 //    test<3,3>();
 
     return 0;
