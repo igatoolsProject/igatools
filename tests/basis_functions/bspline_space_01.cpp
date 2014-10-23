5047274cae880df2418b3bd5d81a1b4fa3e49058
pauletti <spauletti@gmail.com>
2014-10-23 13:52:01 -0300

    updated test, replacing repeated and obsolete ones

--- tests/basis_functions/bspline_space_01.cpp
+++ tests/basis_functions/bspline_space_01.cpp
@@ -17,12 +17,12 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //-+--------------------------------------------------------------------
+
 /*
- *  Test for constructor of bspline spaces
- *  todo: It has to be update to current standard of using and more
- *  dimension independent.
+ *  Test for BSplineSpace constructors
+ *
  *  author: pauletti
- *  date: Feb 19, 2013
+ *  date: 2014-10-23
  *
  */
 
@@ -30,79 +30,62 @@
 
 #include <igatools/basis_functions/new_bspline_space.h>
 
-
-template< int dim_domain, int dim_range, int rank >
-void do_test()
+namespace grid
 {
-    vector< Real > coord_x ;
-    coord_x.push_back(0.0) ;
-    coord_x.push_back(1.0) ;
-    coord_x.push_back(2.0) ;
-    coord_x.push_back(3.0) ;
-    coord_x.push_back(4.0) ;
-
-    vector< Real > coord_y ;
-    coord_y.push_back(5.0) ;
-    coord_y.push_back(6.0) ;
-    coord_y.push_back(7.0) ;
-    coord_y.push_back(8.0) ;
-
-    vector< Real > coord_z ;
-    coord_z.push_back(9.0) ;
-    coord_z.push_back(10.0) ;
-    coord_z.push_back(11.0) ;
-
-    using Space = NewBSplineSpace<dim_domain, dim_range, rank>;
-
-    CartesianProductArray< Real, dim_domain > coord ;
-    TensorIndex<dim_domain> degree ;
+template<int dim>
+shared_ptr<CartesianGrid<dim>>
+uniform(const int n_knots)
+{
+    return CartesianGrid<dim>::create(n_knots);
+}
 
 
-    if (dim_domain == 1)
-    {
-        coord.copy_data_direction(0,coord_x) ;
-        degree[0] = 3 ;
-    }
-    else if (dim_domain == 2)
-    {
-        coord.copy_data_direction(0,coord_x);
-        coord.copy_data_direction(1,coord_y);
-
-        degree[0] = 3 ;
-        degree[1] = 2 ;
-    }
-    else if (dim_domain == 3)
-    {
-        coord.copy_data_direction(0,coord_x);
-        coord.copy_data_direction(1,coord_y);
-        coord.copy_data_direction(2,coord_z);
-
-        degree[0] = 3 ;
-        degree[1] = 2 ;
-        degree[2] = 1 ;
-    }
+};
 
 
-    auto knots = CartesianGrid<dim_domain>::create(coord);
-    auto bspline_space = Space::create(degree, knots) ;
-    bspline_space->print_info(out);
+template<int dim>
+void uniform_degree(const int deg, shared_ptr<CartesianGrid<dim>> grid)
+{
+    OUTSTART
+    auto space = NewBSplineSpace<dim>::create(deg, grid);
+    space->print_info(out);
     out << endl;
+    OUTEND
+}
+
+
+template<int dim>
+void direction_degree(const TensorIndex<dim> &deg,
+                      shared_ptr<CartesianGrid<dim>> grid)
+{
+    OUTSTART
+    auto space = NewBSplineSpace<dim>::create(deg, grid);
+    space->print_info(out);
+    out << endl;
+    OUTEND
 }
 
 
 int main()
 {
-    out.depth_console(10);
-    do_test< 1, 1, 1 >() ;
-    do_test< 1, 2, 1 >() ;
-    do_test< 1, 3, 1 >() ;
+    const int deg = 1;
+    const int n_knots = 2;
+    uniform_degree<0>(deg, grid::uniform<0>(n_knots));
+    uniform_degree<1>(deg, grid::uniform<1>(n_knots));
+    uniform_degree<2>(deg, grid::uniform<2>(n_knots));
+    uniform_degree<3>(deg, grid::uniform<3>(n_knots));
 
-    do_test< 2, 1, 1 >() ;
-    do_test< 2, 2, 1 >() ;
-    do_test< 2, 3, 1 >() ;
+    TensorIndex<0> deg0;
+    direction_degree<0>(deg0, grid::uniform<0>(n_knots));
 
-    do_test< 3, 1, 1 >() ;
-    do_test< 3, 3, 1 >() ;
+    TensorIndex<1> deg1 = {1};
+    direction_degree<1>(deg1, grid::uniform<1>(n_knots));
+
+    TensorIndex<2> deg2 = {2,3};
+    direction_degree<2>(deg2, grid::uniform<2>(n_knots));
+
+    TensorIndex<3> deg3 = {3,4,5};
+    direction_degree<3>(deg3, grid::uniform<3>(n_knots));
 
     return 0;
 }
