703442bf06264fff4a5073c492e11d801104e962
pauletti <spauletti@gmail.com>
2014-11-23 19:11:21 -0300

    partial work

--- tests/base/tensor-co_action_01.cpp
+++ tests/base/tensor-co_action_01.cpp
@@ -38,11 +38,10 @@ void run_test()
     out << "dim: " << dim  << " range: " << range << endl;
     Derivatives<dim, range, rank, 1> DF;
     for (int i = 0; i < dim; ++i)
-        DF[i][i] = double(i+1) ;
-    Derivatives<range, dim, rank, 1> DF_inv;
+    	DF[i][i] = double(i+1) ;
 
-
-    inverse<dim, range>(DF, DF_inv);
+    Real det;
+    auto DF_inv = inverse(DF, det);
 
     auto DF_inv_t = co_tensor(transpose(DF_inv));
 
