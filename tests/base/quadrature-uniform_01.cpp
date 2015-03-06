b36551e8270742621d1d54b1e037406974d2122e
pauletti <spauletti@gmail.com>
2015-03-06 14:57:59 -0300

    rewrote old test

--- tests/base/quadrature-uniform_01.cpp
+++ tests/base/quadrature-uniform_01.cpp
@@ -18,103 +18,63 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //-+--------------------------------------------------------------------
 
-/**
- * This file contains the test for the uniform quadrature scheme,
- * implemented by the QUniform class.
- * \author Sebasian Pauletti (spauletti@gmail.com)
- * \author Massimiliano Martinelli (massimiliano.martinelli@gmail.com)
- * \date 08/04/2013
+/*
+ *  Test for  Uniform quadrature scheme
+ *
+ *  author: pauletti
+ *  date: 2015-03-06
+ *
  */
 
 #include "../tests.h"
 
 #include <igatools/base/quadrature_lib.h>
+#include <igatools/base/array_utils.h>
 
-
-/*
-void do_test_zero()
-{
-    out << "-------------------------------" << endl ;
-    out << "Dimension: 0" << endl;
-    QUniform<0> quad(1) ;
-    quad.print_info( out ) ;
-    out << "-------------------------------" << endl ;
-    out <<endl ;
-}
-//*/
-
-//Isotropic
 template <int dim>
-void do_test()
+void uniform(const int n_pts)
 {
+    OUTSTART
 
+    QUniform<dim> quad(n_pts);
+    quad.print_info(out) ;
 
-    out << "-------------------------------" << endl ;
-    const int max_n_pts = 4;
-    out << "Dimension: " << dim << endl;
-    for (int i = 2; i < max_n_pts; ++i)
-    {
-        out << "Points per direction: " << i << endl << endl ;
-
-        QUniform<dim> quad(i);
-
-        out << "Unit element" << endl ;
-        quad.print_info(out) ;
-        out << endl ;
-
-
-    }
-    out << "-------------------------------" << endl ;
-    out <<endl ;
+    OUTEND
 }
 
 
-//Anisotropic
+
 template <int dim>
-void do_test_aniso()
+void  non_uniform(const int n_pts)
 {
-    array<array<Real,2>,dim> domain_coordinates_new ;
-    for (int j = 0; j < dim; ++j)
-        domain_coordinates_new[j] = {{1.0,3.0}} ;
+    OUTSTART
 
-    out << "-------------------------------" << endl ;
-    const int max_n_pts = 4;
-    out << "Dimension: " << dim << endl;
-    for (int i = 2; i < max_n_pts; ++i)
-    {
-        out << "Points per direction: ";
-        TensorSize<dim> n_points;
-        for (int j = 0; j < dim; ++j)
-        {
-            n_points[j] = i + j;
-            out << i+j << " ";
-        }
-        out << endl << endl ;
+    const TensorSize<dim> n_points (arr::sequence<dim>(n_pts));
+    QUniform<dim> quad(n_points);
+    quad.print_info(out);
 
-        QUniform<dim> quad(n_points);
-
-        out << "Unit element" << endl ;
-        quad.print_info(out) ;
-        out << endl ;
-
-    }
-    out << "-------------------------------" << endl ;
-    out <<endl ;
+    OUTEND
 }
 
 
 
-int main(int argc, char *argv[])
+
+int main()
 {
-//  do_test_zero() ;
+    const int max_n_pts = 4;
+    for (int n = 2; n <  max_n_pts; ++n)
+    {
+        uniform<0>(n);
+        uniform<1>(n);
+        uniform<2>(n);
+        uniform<3>(n);
 
-    do_test<1>();
-    do_test<2>();
-    do_test<3>();
-
-    do_test_aniso<1>();
-    do_test_aniso<2>();
-    do_test_aniso<3>();
+        non_uniform<0>(n);
+        non_uniform<1>(n);
+        non_uniform<2>(n);
+        non_uniform<3>(n);
+    }
 
     return 0;
 }
+
