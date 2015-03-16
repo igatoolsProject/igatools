5a8ca9e2d9469be3095e330f4a52e57c7de0c5a8
Massimiliano Martinelli <massimiliano.martinelli@gmail.com>
2015-03-16 16:50:55 +0100

    Modified a test in order to test the use of the dofs value filtering

--- tests/basis_functions/bspline_iterator_01.cpp
+++ tests/basis_functions/bspline_iterator_01.cpp
@@ -80,20 +80,85 @@ void bspline_iterator(const int deg = 2,const int n_qp = 3)
 }
 
 
+template<int dim, int k=dim, int range = 1, int rank = 1>
+void bspline_iterator_active_dofs(const int deg = 2,const int n_qp = 3)
+{
+    OUTSTART
+
+    auto grid = CartesianGrid<dim>::create();
+    using RefSpace = ReferenceSpace<dim, range, rank>;
+    using RefElementHandler = typename RefSpace::ElementHandler;
+    using Space = BSplineSpace<dim, range, rank>;
+    using ElementHandler = typename Space::ElementHandler;
+    auto space = Space::create(deg, grid);
+
+    auto dof_distribution = space->get_dof_distribution();
+    dof_distribution->add_dofs_property(DofProperties::active);
+    for (const auto dof: dof_distribution->get_dofs_view())
+        if (dof % 2 == 0)
+            dof_distribution->set_dof_property_status(DofProperties::active,dof,true);
+
+
+    QGauss<k> quad(n_qp);
+    auto flag = ValueFlags::value|ValueFlags::gradient
+                |ValueFlags::hessian;
+    std::shared_ptr<RefElementHandler> cache = ElementHandler::create(space);
+    cache->reset(flag, quad);
+
+    auto elem = space->begin();
+    cache->init_element_cache(elem);
+
+    for (auto &s_id : UnitElement<dim>::template elems_ids<k>())
+    {
+        cache->fill_element_cache(elem);
+
+        out << "Sub Element: " << s_id << endl;
+        auto values    = elem->template get_values<0,k>(s_id,DofProperties::active);
+        auto gradients = elem->template get_values<1,k>(s_id,DofProperties::active);
+        auto hessians  = elem->template get_values<2,k>(s_id,DofProperties::active);
+
+        out.begin_item("Values basis functions:");
+        values.print_info(out);
+        out.end_item();
+
+        out.begin_item("Gradients basis functions:");
+        gradients.print_info(out);
+        out.end_item();
+
+        out.begin_item("Hessians basis functions:");
+        hessians.print_info(out);
+        out.end_item();
+    }
+}
+
+
 int main()
 {
     out.depth_console(0);
 
     bspline_iterator<1,1,1>();
+    bspline_iterator_active_dofs<1,1,1>();
+
     bspline_iterator<1,1,2>();
+    bspline_iterator_active_dofs<1,1,2>();
+
     bspline_iterator<1,1,3>();
+    bspline_iterator_active_dofs<1,1,3>();
 
     bspline_iterator<2,2,1>();
+    bspline_iterator_active_dofs<2,2,1>();
+
     bspline_iterator<2,2,2>();
+    bspline_iterator_active_dofs<2,2,2>();
+
     bspline_iterator<2,2,3>();
+    bspline_iterator_active_dofs<2,2,3>();
 
     bspline_iterator<3,3,1>();
+    bspline_iterator_active_dofs<3,3,1>();
+
     bspline_iterator<3,3,3>();
+    bspline_iterator_active_dofs<3,3,3>();
 
     return 0;
 }
