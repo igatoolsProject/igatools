46ca753d0c8ce81e22577a3e8602fe0af36e71be
pauletti <spauletti@gmail.com>
2015-04-22 16:32:06 -0300

    checking test for accurate results and simplification

--- tests/basis_functions/phys_space_iterator_03.cpp
+++ tests/basis_functions/phys_space_iterator_03.cpp
@@ -27,86 +27,71 @@
  *
  */
 
-#include "../tests.h"
-
-#include <igatools/base/quadrature_lib.h>
-#include <igatools/base/function_lib.h>
 #include <igatools/base/identity_function.h>
-
-#include <igatools/basis_functions/bspline_space.h>
-#include <igatools/basis_functions/physical_space.h>
-#include <igatools/basis_functions/physical_space_element.h>
-#include <igatools/basis_functions/phys_space_element_handler.h>
+#include <igatools/base/function_lib.h>
+#include "phys_space_iterator.h"
 
 
-template<int dim, int codim=0>
-auto
-create_function(shared_ptr<CartesianGrid<dim>> grid)
-{
-    using Function = functions::BallFunction<dim>;
-    return Function::create(grid, IdentityFunction<dim>::create(grid));
-}
 
-
-template <int dim, int order = 0, int range=1, int rank=1, int codim = 0>
-void elem_values(const int n_knots = 2, const int deg=1)
+template<int dim, int sub_dim = dim>
+void
+ball_map(const int n_knots, const int deg, const string prop=DofProperties::active,
+             const bool use_bdry=true)
 {
     OUTSTART
 
-    const int k = dim;
-    using BspSpace = BSplineSpace<dim, range, rank>;
-    using RefSpace = ReferenceSpace<dim, range,rank>;
-    using Space = PhysicalSpace<dim,range,rank,codim, Transformation::h_grad>;
-    using ElementHandler = typename Space::ElementHandler;
+    BBox<dim> box;
+    box[0] = {0.5, 1};
+    for (int i=1; i<dim; ++i)
+    box[i] = {0., M_PI/4.};
 
-    auto grid  = CartesianGrid<dim>::create(n_knots);
-
-    auto ref_space = BspSpace::create(deg, grid);
-    auto map_func = create_function(grid);
-
-    auto space = Space::create(ref_space, map_func);
-
+    auto grid  = CartesianGrid<dim>::create(box, n_knots);
+    auto map = functions::BallFunction<dim>::create(grid,IdentityFunction<dim>::create(grid));
+    auto space = create_space<dim>(grid, map, deg);
     const int n_qp = 2;
-    auto quad = QGauss<k>(n_qp);
-    auto flag = ValueFlags::value |
-                ValueFlags::gradient |
-                ValueFlags::hessian |
-                ValueFlags::point;
-
-    ElementHandler sp_values(space);
-    sp_values.template reset<k> (flag, quad);
-
-    auto elem = space->begin();
-    auto end = space->end();
-    sp_values.init_element_cache(elem);
-
-    for (; elem != end; ++elem)
-    {
-        sp_values.fill_element_cache(elem);
-
-        out << "Basis values: " << endl;
-        elem->template get_basis<_Value, k>(0,DofProperties::active).print_info(out);
-        out << endl;
-
-        out << "Basis gradients: " << endl;
-        elem->template get_basis<_Gradient, k>(0,DofProperties::active).print_info(out);
-        out << endl;
-
-        out << "Basis hessians: " << endl;
-        elem->template get_basis<_Hessian, k>(0,DofProperties::active).print_info(out);
-    }
+    elem_values<dim, sub_dim>(space, n_qp, prop, use_bdry);
 
     OUTEND
 }
 
+template<int dim, int sub_dim = dim>
+void
+ball_map_prop(const int n_knots, const int deg, const bool use_bdry=true)
+{
+    OUTSTART
+
+    BBox<dim> box;
+    box[0] = {0.5, 1};
+    for (int i=1; i<dim; ++i)
+        box[i] = {0., M_PI/4.};
+
+    auto grid  = CartesianGrid<dim>::create(box, n_knots);
+
+    auto map = functions::BallFunction<dim>::create(grid,IdentityFunction<dim>::create(grid));
+    auto space = create_space_prop<dim>(grid, map, deg);
+    const int n_qp = 1;
+    elem_values<dim, sub_dim>(space, n_qp, DofProp::interior, use_bdry);
+    elem_values<dim, sub_dim>(space, n_qp, DofProp::dirichlet, use_bdry);
+    elem_values<dim, sub_dim>(space, n_qp, DofProp::neumman, use_bdry);
+    OUTEND
+}
+
+
+
+
+
 
 int main()
 {
-    out.depth_console(10);
 
-    elem_values<1>();
-    elem_values<2>();
-    elem_values<3>();
+  //  ball_map_prop<1,1>(2,1,true);
 
+
+  //  ball_map<1,1>(2,1);
+   // ball_map<2,1>(3,2);
+  //  ball_map<3,3>(2,1);
+
+    //ball_map_prop<2,1>(3,1);
+    ball_map_prop<2,1>(3,2, false);
     return 0;
 }
