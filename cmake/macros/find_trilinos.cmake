33881fc698ace951514de3a5c0e83fab3ffd974e
antolin <pablo.antolin.sanchez@gmail.com>
2015-05-20 10:59:51 +0200

    Modifying find_trilinos for allowing versions higher than 11

--- cmake/macros/find_trilinos.cmake
+++ cmake/macros/find_trilinos.cmake
@@ -23,7 +23,7 @@
 macro(find_trilinos)
   set(Trilinos_PREFIX $ENV{TRILINOS_PREFIX} CACHE LOCATION 
     "Location where Trilinos library is installed")
-  find_package(Trilinos 11 REQUIRED PATHS ${Trilinos_PREFIX})
+  find_package(Trilinos REQUIRED PATHS ${Trilinos_PREFIX})
   message(STATUS "Found Trilinos:  version ${Trilinos_VERSION}.")
   if (NOT (Trilinos_VERSION VERSION_GREATER 11.6))
     message(FATAL_ERROR "Trilinos 11.6.1 or greater is required.")
