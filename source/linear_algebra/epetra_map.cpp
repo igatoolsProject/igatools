706c464af5b871950c3fecd8ec9402eca2ec58e7
Massimiliano Martinelli <massimiliano.martinelli@gmail.com>
2015-07-02 16:40:09 +0200

    In the namespace EpetraTools, added an interface to create an Epetra_Map

--- source/linear_algebra/epetra_map.cpp
+++ source/linear_algebra/epetra_map.cpp
@@ -18,50 +18,21 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //-+--------------------------------------------------------------------
 
-#ifndef __EPETRA_MAP_H_
-#define __EPETRA_MAP_H_
-
-#include <igatools/base/config.h>
-
-#include <igatools/linear_algebra/dense_vector.h>
-#include <igatools/linear_algebra/dense_matrix.h>
-#include <igatools/utils/safe_stl_vector.h>
-
-#include <Epetra_SerialComm.h>
-#include <Epetra_Map.h>
-
-#include <set>
+#include <igatools/linear_algebra/epetra_map.h>
 
 IGA_NAMESPACE_OPEN
 
 namespace EpetraTools
 {
-using Comm = Epetra_Comm;
-using CommPtr = std::shared_ptr<const Comm>;
 
-using Map = Epetra_Map;
-using MapPtr = std::shared_ptr<Map>;
-
-
-/**
- * Create an Epetra_Map object (wrapped by a shared pointer) from a set of @dofs.
- */
 MapPtr
-create_map(const std::set<Index> &dofs,Comm &comm);
-
-template<class Space>
-MapPtr create_map(const Space &space,
-                  const std::string &property,
-                  Comm &comm)
+create_map(const std::set<Index> &dofs,
+           Comm &comm)
 {
-    const auto &dof_dist = *space.get_ptr_const_dof_distribution();
-
-    return create_map(dof_dist.get_dofs_id_same_property(property),comm);
+    SafeSTLVector<Index> dofs_vec(dofs.begin(), dofs.end());
+    return std::make_shared<Map>(-1, dofs_vec.size(), dofs_vec.data(), 0, comm);
 }
-
 
 };
 
 IGA_NAMESPACE_CLOSE
-
-#endif
