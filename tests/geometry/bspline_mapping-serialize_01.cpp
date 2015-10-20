06512ad208ad3d8ede01e550f7746c3cdd3f9d36
Massimiliano Martinelli <massimiliano.martinelli@gmail.com>
2015-10-20 10:16:38 +0200

    Partial work in tests/geometry/bspline_mapping-serialize_01.cpp

--- tests/geometry/bspline_mapping-serialize_01.cpp
+++ tests/geometry/bspline_mapping-serialize_01.cpp
@@ -28,34 +28,33 @@
 
 #include "../tests.h"
 
-#include <igatools/geometry/mapping.h>
-#include <igatools/geometry/mapping_element.h>
-#include <igatools/functions/ig_function.h>
+#include <igatools/geometry/domain.h>
+#include <igatools/functions/ig_grid_function.h>
 #include <igatools/base/quadrature_lib.h>
 #include <igatools/basis_functions/bspline_space.h>
 #include <igatools/basis_functions/bspline_element.h>
 #include <igatools/functions/function_element.h>
+#include <igatools/linear_algebra/epetra_vector.h>
 
 using namespace EpetraTools;
 
 
 template <int dim,int codim>
-void serialize_deserialize(std::shared_ptr< IgFunction<dim,0, dim+codim,1> > F)
+void serialize_deserialize(std::shared_ptr< IgGridFunction<dim,dim+codim> > F)
 {
-  out.begin_item("Original IgFunction:");
+  out.begin_item("Original IgGridFunction:");
   F->print_info(out);
   out.end_item();
 
   std::string template_strdetails = "_dim" + std::to_string(dim) + "_codim" + std::to_string(codim);
-  std::string filename = "ig_function" + template_strdetails + ".xml";
-  std::string tag_name = "IgFunction" + template_strdetails;
+  std::string filename = "ig_grid_function" + template_strdetails + ".xml";
+  std::string tag_name = "IgGridFunction" + template_strdetails;
   {
     // serialize the IgFunction object to an xml file
     std::ofstream xml_ostream(filename);
     OArchive xml_out(xml_ostream);
-//        xml_out.template register_type<BSplineSpace<dim,dim+codim>>();
 
-    xml_out << boost::serialization::make_nvp(tag_name.c_str(),F);
+    xml_out << F;
     xml_ostream.close();
   }
 
@@ -64,12 +63,11 @@ void serialize_deserialize(std::shared_ptr< IgFunction<dim,0, dim+codim,1> > F)
     // de-serialize the IgFunction object from an xml file
     std::ifstream xml_istream(filename);
     IArchive xml_in(xml_istream);
-//        xml_in.template register_type<BSplineSpace<dim,dim+codim>>();
 
-    xml_in >> BOOST_SERIALIZATION_NVP(F);
+    xml_in >> F;
     xml_istream.close();
   }
-  out.begin_item("IgFunction after serialize-deserialize:");
+  out.begin_item("IgGridFunction after serialize-deserialize:");
   F->print_info(out);
   out.end_item();
 //*/
@@ -85,14 +83,13 @@ void bspline_map(const int deg = 1)
   const int sub_dim = dim;
   using Space = BSplineSpace<dim, dim+codim>;
   using RefSpace = ReferenceSpace<dim, dim+codim>;
-  using Function = IgFunction<dim,0,dim+codim,1>;
-  using Mapping   = Mapping<dim, codim>;
+  using Function = IgGridFunction<dim,dim+codim>;
+  using Mapping = Domain<dim, codim>;
 
   auto grid = Grid<dim>::create(2);
   auto space = Space::create(deg, grid);
 
-  auto c_p = EpetraTools::create_vector(*space, "active",Epetra_SerialComm());
-  auto &control_pts = *c_p;
+  IgCoefficients control_pts;
 
   if (dim == 1)
   {
@@ -156,7 +153,7 @@ void bspline_map(const int deg = 1)
 
   }
 
-  auto F = Function::create(space, c_p);
+  auto F = Function::create(space, control_pts);
 
 
   serialize_deserialize<dim,codim>(F);
@@ -165,8 +162,10 @@ void bspline_map(const int deg = 1)
   auto map = Mapping::create(F);
 
   auto quad = QGauss<dim>(3);
-  auto flag =  ValueFlags::value| ValueFlags::gradient
-               | ValueFlags::hessian;
+  using Flags = grid_function_element::Flags;
+  auto flag =  Flags::D0|
+		       Flags::D1|
+			   Flags::D2;
 
   map->template reset<sub_dim>(flag, quad);
 
@@ -175,14 +174,17 @@ void bspline_map(const int deg = 1)
   const int s_id = 0;
 
   map->template init_cache<sub_dim>(elem);
+  using D0 = grid_function_element::template _D<0>;
+  using D1 = grid_function_element::template _D<1>;
+  using D2 = grid_function_element::template _D<2>;
   for (; elem != end; ++elem)
   {
     map->template fill_cache<sub_dim>(elem, s_id);
     out << "Values (x1,x2,...):" << endl;
-    elem->template get_values<_Value,sub_dim>(s_id).print_info(out);
+    elem->template get_values<D0,sub_dim>(s_id).print_info(out);
     out << endl;
     out << "Gradients:" << endl;
-    elem->template get_values<_Gradient,sub_dim>(s_id).print_info(out);
+    elem->template get_values<D1,sub_dim>(s_id).print_info(out);
     out << endl;
 //        elem->template get_values<2,sub_dim>(s_id).print_info(out);
 //        out << endl;
