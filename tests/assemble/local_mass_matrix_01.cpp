8b1d3a4040ce956a2e1b9d90702b2fd1abe3aa5e
Massimiliano Martinelli <massimiliano.martinelli@gmail.com>
2015-11-06 13:45:09 +0100

    Added a test for the local mass matrix computation

---
+++ tests/assemble/local_mass_matrix_01.cpp
@@ -0,0 +1,101 @@
+//-+--------------------------------------------------------------------
+// Igatools a general purpose Isogeometric analysis library.
+// Copyright (C) 2012-2015  by the igatools authors (see authors.txt).
+//
+// This file is part of the igatools library.
+//
+// The igatools library is free software: you can use it, redistribute
+// it and/or modify it under the terms of the GNU General Public
+// License as published by the Free Software Foundation, either
+// version 3 of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//-+--------------------------------------------------------------------
+
+/*
+ *  Test the assembling of local mass matrix using a BSpline basis
+ *
+ *  author: martinelli
+ *  date: Nov 06, 2015
+ *
+ */
+
+#include "../tests.h"
+
+#include <igatools/basis_functions/phys_space_element_handler.h>
+#include <igatools/basis_functions/bspline.h>
+#include <igatools/basis_functions/bspline_element_handler.h>
+#include <igatools/basis_functions/bspline_element.h>
+#include <igatools/base/quadrature_lib.h>
+
+//#include <igatools/linear_algebra/dense_matrix.h>
+
+
+template<int dim>
+void loc_mass_matrix(const int n_knots, const int deg)
+{
+
+  OUTSTART
+
+  out.begin_item("local_mass_matrix<" + std::to_string(dim) + ">");
+
+  auto grid = Grid<dim>::create(n_knots);
+  using Basis = BSpline<dim>;
+  auto basis = Basis::create(SplineSpace<dim>::create(deg, grid)) ;
+
+  auto elem_handler = basis->create_cache_handler();
+
+
+  using Flags = space_element::Flags;
+  auto flag = Flags::value | Flags::w_measure;
+  elem_handler->template set_flags<dim>(flag);
+
+  auto elem           = basis->begin();
+  const auto elem_end = basis->end();
+
+  auto quad = QGauss<dim>::create(deg+1);
+  elem_handler->init_element_cache(elem,quad);
+
+  int elem_id = 0;
+  for (; elem != elem_end; ++elem, ++elem_id)
+  {
+    out.begin_item("Element " + std::to_string(elem_id));
+
+    out << "Element ID: " << elem->get_index() << std::endl;
+
+    elem_handler->fill_element_cache(elem);
+
+    auto loc_mat = elem->integrate_u_v();
+    out.begin_item("Mass matrix:");
+    loc_mat.print_info(out);
+    out.end_item();
+
+    out.end_item();
+  }
+
+  out.end_item();
+
+  OUTEND
+
+}
+
+
+
+
+int main()
+{
+  const int n_knots = 6;
+  const int deg = 1;
+
+  loc_mass_matrix<1>(n_knots, deg);
+  loc_mass_matrix<2>(n_knots, deg);
+  loc_mass_matrix<3>(n_knots, deg);
+
+  return  0;
+}
