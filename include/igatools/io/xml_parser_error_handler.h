2aab23fe301616b67dad77e32dba5763245f63bb
Pablo Antolin <pablo.antolin.sanchez@gmail.com>
2015-11-22 10:55:00 +0100

    Work on XMLParserErrorHandler

--- include/igatools/io/xml_parser_error_handler.h
+++ include/igatools/io/xml_parser_error_handler.h
@@ -23,43 +23,28 @@
 #if XML_IO
 
 #include <igatools/base/config.h>
-#include <igatools/utils/safe_stl_vector.h>
 
-#include <xercesc/sax/ErrorHandler.hpp>
-
+#include <xercesc/sax/ParserErrorHandler.hpp>
 
 IGA_NAMESPACE_OPEN
 
 
 /**
- * Forward declarations.
- */
-//class InputTreeLog;
-class LogManager;
-
-
-/**
- * @brief Manage runtime errors occurred during the parser of the input files.
+ * @brief Manage runtime errors occurred during the parsing process of
+ * input files.
  *
- * This class is in charge of managing the runtime error than can occurred
- * during the parser of the input files.
+ * This class is in charge of managing the runtime errors than can
+ * occurred during the parsing process of input files.
  *
- * The class derives from the @p xercesc class @ref ErrorHandler, in such a way,
- * it takes care of error messages thrown by @p xercesc parser.
- *
- * It also allows to throw error occurred during the parsing process by means
- * of the functions @ref throw_error. This function makes use of the
- * @ref InputTreeLog object contained inside to produce richer error messages.
- *
- * This class can track different nested input files, with their respective
- * @ref InputTreeLog
+ * The class derives from the @p xercesc class @ref ParserErrorHandler, in such
+ * a way, it takes care of error messages thrown by @p xercesc parser.
  *
  * @attention When an error or warning is thrown, an exception raises,
  * and the execution is automatically finished.
  *
  * \author antolin, 2015
  */
-class XMLFileParserHandler : public xercesc::ErrorHandler
+class XMLParserErrorHandler : public xercesc::ParserErrorHandler
 {
 private:
 
@@ -68,7 +53,7 @@ private:
 
   /// Type for the current class.
   /** Type for the current class. */
-  typedef XMLFileParserHandler Self_;
+  typedef XMLParserErrorHandler Self_;
 
   /// Type for a shared pointer of the current class.
   /** Type for a shared pointer of the current class. */
@@ -76,49 +61,19 @@ private:
 
   ///@}
 
-
+public:
   /** @name Constructors, destructor, assignment operators and creators */
   ///@{
 
   /**
-   * @brief Constructor taking a input tree log and the file path.
-   *
-   * Constructor taking a input tree log and the file path.
-   *
-   * @param[in] log Input tree log to be used for logging purposes.
-   * @param[in] file_path Path of the file being parsed.
-   */
-//  XMLFileParserHandler(const std::shared_ptr<InputTreeLog> log,
-//                     const std::shared_ptr<LogManager> log_manager,
-//                     const std::string &file_path);
-  XMLFileParserHandler(const std::shared_ptr<LogManager> log_manager,
-                     const std::string &file_path);
-
-public:
-  /**
-   * @brief Returns a new instance wrapped into a @p shared_ptr
-   *
-   * Builds and returns a new instance of the class wrapped into a shared pointer.
-   * It uses the above defined constructor for @p log and @p file_path.
-   *
-   * @param[in] log Input tree log to be used for logging purposes.
-   * @param[in] file_path Path of the file being parsed.
-   */
-//  static SelfPtr_ create(const std::shared_ptr<InputTreeLog> log,
-//                         const std::shared_ptr<LogManager> log_manager,
-//                         const std::string &file_path);
-  static SelfPtr_ create(const std::shared_ptr<LogManager> log_manager,
-                         const std::string &file_path);
-
-
-private:
-  /**
-   * @brief Deleted default constructor.
+   * @brief Default constructor
    *
    * Default constructor.
-   * @note Deleted: not allowed.
+   * @return New instance of the class.
    */
-  XMLFileParserHandler() = delete;
+  XMLParserErrorHandler() = default;
+
+private:
 
   /**
    * @brief Deleted copy constructor.
@@ -126,7 +81,7 @@ private:
    * Copy constructor.
    * @note Deleted: not allowed.
    */
-  XMLFileParserHandler(const XMLFileParserHandler &) = delete;
+  XMLParserErrorHandler(const XMLParserErrorHandler &) = delete;
 
   /**
    * @brief Deleted move constructor.
@@ -134,7 +89,7 @@ private:
   * Move constructor.
   * @note Deleted: not allowed.
   */
-  XMLFileParserHandler(XMLFileParserHandler &&) = delete;
+  XMLParserErrorHandler(XMLParserErrorHandler &&) = delete;
 
   /**
    * @brief Deleted copy assignment operator.
@@ -142,7 +97,7 @@ private:
    * Copy assignment operator.
    * @note Deleted: not allowed.
    */
-  XMLFileParserHandler &operator= (const XMLFileParserHandler &) = delete;
+  XMLParserErrorHandler &operator= (const XMLParserErrorHandler &) = delete;
 
   /**
    * @brief Deleted move assignment operator.
@@ -150,54 +105,28 @@ private:
    * Move assignment operator.
    * @note Deleted: not allowed.
    */
-  XMLFileParserHandler &operator= (XMLFileParserHandler &&) = delete;
+  XMLParserErrorHandler &operator= (XMLParserErrorHandler &&) = delete;
 
   ///@}
 
-private:
-
-  /// Input tree logs used for throwing error messages.
-  /**
-   * Vector of input tree logs used for throwing error messages.
-   * Every log corresponds to an input file.
-   */
-//  SafeSTLVector<std::shared_ptr<InputTreeLog>> logs_;
-
-  /// Log manager used for outputting messages.
-  /** Log manager used for outputting messages. */
-  const std::shared_ptr<LogManager> log_manager_;
-
-  /// Paths of the input files being parsed.
-  /**
-   *  Paths of the input files being parsed.
-   *
-   *  They are store in order, first vector entry corresponds to the main
-   *  input file. Subsequent elements, to the nested input files.
-   */
-  SafeSTLVector<std::string> file_paths_;
-
-
-  /** @name xercesc ErrorHandler pure virtual methods */
+  /** @name xercesc ParserErrorHandler pure virtual methods */
   ///@{
-
-public:
 
   /**
    * @brief Receive notification of a recoverable error.
-   * This corresponds to the definition of "error" in section 1.2 of the W3C
-   * XML 1.0 Recommendation. For example, a validating parser would use this
-   * callback to report the violation of a validity constraint. The default
-   * behaviour is to take no action.
+   * This corresponds to the definition of "error" in section 1.2 of the
+   * W3C XML 1.0 Recommendation. For example, a validating parser would
+   * use this callback to report the violation of a validity constraint.
+   * The default behaviour is to take no action.
    *
    * The SAX parser must continue to provide normal parsing events after
    * invoking this method: it should still be possible for the application
    * to process the document through to the end. If the application cannot
-   * do so, then the parser should report a fatal error even if the XML 1.0
-   * recommendation does not require it to do so.
+   * do so, then the parser should report a fatal error even if the XML
+   * 1.0 recommendation does not require it to do so.
    *
-   * The error message is thrown by means of the method @ref error.
-   * It contains information about the input file, line and column, and
-   * the XML tree location.
+   * An error message is thrown containing additional information about
+   * the line and the column where it occurred.
    *
    * @param[in] ex The warning information encapsulated in a SAX parse
    *               exception.
@@ -206,7 +135,7 @@ public:
    *                         exception.
    *
    * @note The documentation has been partially extracted from the parent
-   * <tt>xercesc ErrorHandler</tt> class.
+   * <tt>xercesc ParserErrorHandler</tt> class.
    */
   virtual void error(const xercesc::SAXParseException &ex) override final;
 
@@ -220,12 +149,12 @@ public:
    * invoking this method: it should still be possible for the application
    * to process the document through to the end.
    *
-   * \attention Currently the warning message is treated as an error message,
-   * i.e. the execution will be finished after throwing the message.
+   * \attention Currently the warning message is treated as an error
+   * message, i.e. the execution will be finished after throwing the
+   * message.
    *
-   * The error message is thrown by means of the method @ref error.
-   * It contains information about the input file, line and column, and
-   * the XML tree location.
+   * An error message is thrown containing additional information about
+   * the line and the column where it occurred.
    *
    * @param[in] ex The warning information encapsulated in a SAX parse
    *               exception.
@@ -234,7 +163,7 @@ public:
    *                         exception.
    *
    * @note The documentation has been partially extracted from the parent
-   * <tt>xercesc ErrorHandler</tt> class.
+   * <tt>xercesc ParserErrorHandler</tt> class.
    */
   virtual void warning(const xercesc::SAXParseException &ex) override final;
 
@@ -247,19 +176,21 @@ public:
    *
    * The application must assume that the document is unusable after the
    * parser has invoked this method, and should continue (if at all) only
-   * for the sake of collecting addition error messages: in fact, SAX parsers are free to stop reporting any other events once this method has been invoked.
+   * for the sake of collecting addition error messages: in fact, SAX
+   * parsers are free to stop reporting any other events once this method
+   * has been invoked.
    *
-   * The error message is thrown by means of the method @ref error.
-   * It contains information about the input file, line and column, and
-   * the XML tree location.
+   * An error message is thrown containing additional information about
+   * the line and the column where it occurred.
    *
-   * @param[in] ex The error information encapsulated in a SAX parse exception.
+   * @param[in] ex The error information encapsulated in a SAX parse
+   *               exception.
    *
    * @exception SAXException  Any SAX exception, possibly wrapping another
    *                          exception.
    *
    * @note The documentation has been partially extracted from the parent
-   * <tt>xercesc ErrorHandler</tt> class.
+   * <tt>xercesc ParserErrorHandler</tt> class.
    * @note This function is doing nothing currently.
    */
   virtual void fatalError(const xercesc::SAXParseException &ex) override final;
@@ -271,151 +202,16 @@ public:
    * defaults each time the Error handler is begun.
    *
    * @note The documentation has been extracted from the parent
-   * <tt>xercesc ErrorHandler</tt> class.
+   * <tt>xercesc ParserErrorHandler</tt> class.
    *
    * @note The documentation has been extracted from the parent
-   * <tt>xercesc ErrorHandler</tt> class.
-   * @note This function is doing nothing currently.
+   * <tt>xercesc ParserErrorHandler</tt> class.
+   *
+   * @note Currently, this function is doing nothing.
    */
   virtual void resetErrors() override final;
 
   ///@}
-
-
-  /**
-   * @brief Throws an exception with an error message if the @p condition
-   * is not fulfilled.
-   *
-   * Throws an exception error if the @p condition is not fulfilled.
-   * The error message is generated by included the @p file_path_,
-   * the @p line_number and @p column_number, the XML tree location provided
-   * by the last of the @p logs_ and the provided @p message.
-   *
-   * @attention After the error message, the execution is finished.
-   *
-   * @param[in] condition If it is not fulfilled, the error is thrown.
-   * @param[in] message Information for the error message.
-   * @param[in] line_number Line number in the input file where the error
-   *            occurs. The -1 default value is provided, if this value
-   *            is not overridden, the line number is set as @p unknown
-   *            in the error message.
-   * @param[in] column_number Column number in the input file where the error
-   *            occurs. The -1 default value is provided, if this value
-   *            is not overridden, the column number is set as @p unknown
-   *            in the error message.
-   */
-  void error(const bool condition,
-             const std::string &message,
-             const int &line_number = -1,
-             const int &column_number = -1) const;
-
-  /**
-   * @brief Throws an exception with an error message.
-   *
-   * Throws an exception error.
-   * The error message is generated by included the @p file_path_,
-   * the XML tree location provided by the last of the @p logs_ and the
-   * provided @p message.
-   *
-   * @attention After the error message, the execution is finished.
-   *
-   * @param[in] message Information for the error message.
-   */
-  void error(const std::string &msg) const;
-
-  /**
-   * @brief Throws an exception with an error message.
-   *
-   * Throws an exception error.
-   * The error message is generated by included the @p file_path_,
-   * the @p line_number and @p column_number, the XML tree location provided
-   * by the last of the @p logs_ and the provided @p message.
-   *
-   * @attention After the error message, the execution is finished.
-   *
-   * @param[in] message Information for the error message.
-   * @param[in] line_number Line number in the input file where the error
-   *            occurs. The -1 default value is provided, if this value
-   *            is not overridden, the line number is set as @p unknown
-   *            in the error message.
-   * @param[in] column_number Column number in the input file where the error
-   *            occurs. The -1 default value is provided, if this value
-   *            is not overridden, the column number is set as @p unknown
-   *            in the error message.
-   */
-  void error(const std::string &msg,
-             const int &line_number,
-             const int &column_number) const;
-
-
-  /** @name Functions for adding/removing tags from the Input Tree Logs */
-  ///@{
-  /**
-   * @brief Adds an @p id to the last tag name defined.
-   *
-   * Adds an @p id number to the last tag name defined in the last of the
-   * @ref logs_.
-   *
-   * @param[in] id Number to be added to the last tag name.
-   *
-   * @note It calls the function @p add_id of the @ref @InputTreeLog.
-   */
-  void add_id(const Index &id);
-
-  /**
-   * @brief Appends an @p tag_name at the end of the tree.
-   *
-   * Appends a new @p tag_name at the end of the tag names tree in the last
-   * of the @ref logs_.
-   *
-   * @param[in] tag_name Name of the tag to be included at the beginning of
-   *                     the tree.
-   *
-   * @note It calls the function @p add_tag_name of the @ref @InputTreeLog.
-   */
-  void add_tag_name(const std::string &tag_name);
-
-  /**
-   * @brief Removes the last tag name of the tree.
-   *
-   * Removes the last tag name appended to the tree in the last
-   * of the @ref logs_.
-   *
-   * @note It calls the function @p pop_tag_name of the @ref @InputTreeLog.
-   */
-  void pop_tag_name();
-  ///@}
-
-  /**
-   * @brief Appends a new nested input file with its corresponding XML tag.
-   *
-   * Appends a new nested input file with its corresponding XML tag for the
-   * main XML element of the file.
-   *
-   * @param[in] file_path Path of the file being parsed.
-   * @param[in] tag_name Name of the tag to be included at the beginning of
-   *                     the tree.
-   *
-   * @note It creates a new @p log and appends it to @ref logs_.
-   */
-  void add_file(const std::string &file_path, const std::string &tag_name);
-
-  /**
-   * @brief Removes the last input file and tree log.
-   *
-   * Removes the last input file of @ref file_paths_ and the last log of
-   * @ref logs_.
-   */
-  void pop_file();
-
-  /**
-   * @brief Prints information of the class to the log stream.
-   *
-   * Prints information of the class to the log stream.
-   *
-   * @param[in] out Log in which the information is streamed.
-   */
-  void print_info(iga::LogStream &out) const;
 
 };
 
@@ -424,4 +220,4 @@ IGA_NAMESPACE_CLOSE
 
 #endif // XML_IO
 
-#endif // XML_IGATOOLS_OBJECTS_PARSER_H_
+#endif // XML_PARSER_ERROR_HANDLER_H_
--- source/io/xml_parser_error_handler.cpp
+++ source/io/xml_parser_error_handler.cpp
@@ -21,244 +21,58 @@
 
 #if XML_IO
 
-//#include <isolde/base/exceptions.h>
-//#include <isolde/io/input_tree_log.h>
-//#include <isolde/io/log_manager.h>
-
 #include <xercesc/sax/HandlerBase.hpp>
 
-
-using namespace iga;
 using namespace xercesc;
-using std::string;
-using std::endl;
-using std::to_string;
-using std::shared_ptr;
 
 IGA_NAMESPACE_OPEN
 
 
-//XMLFileParserHandler::
-//XMLFileParserHandler(const shared_ptr<InputTreeLog> log,
-//                   const std::shared_ptr<LogManager> log_manager,
-//                   const string &file_path)
-//  :
-//  logs_(1, log),
-//  log_manager_(log_manager),
-//  file_paths_(1, file_path)
-//{
-////  IsoldeAssert(log != nullptr, ExcNullPtr());
-////  IsoldeAssert(log_manager_ != nullptr, ExcNullPtr());
-//}
-
-XMLFileParserHandler::
-XMLFileParserHandler(const std::shared_ptr<LogManager> log_manager,
-                    const string &file_path)
-  :
-//  logs_(1, log),
-  log_manager_(log_manager),
-  file_paths_(1, file_path)
-{
-//  IsoldeAssert(log != nullptr, ExcNullPtr());
-//  IsoldeAssert(log_manager_ != nullptr, ExcNullPtr());
-}
-//
-//
-//
-//auto
-//XMLFileParserHandler::
-//create(const shared_ptr<InputTreeLog> log,
-//       const std::shared_ptr<LogManager> log_manager,
-//       const string &file_path) -> SelfPtr_
-//{
-//  return SelfPtr_(new Self_(log, log_manager, file_path));
-//}
-
-
-
 auto
-XMLFileParserHandler::
-create(const std::shared_ptr<LogManager> log_manager,
-       const string &file_path) -> SelfPtr_
+XMLParserErrorHandler::
+create() -> SelfPtr_
 {
-  return SelfPtr_(new Self_(log_manager, file_path));
+  return SelfPtr_(new Self_());
 }
 
 
 void
-XMLFileParserHandler::
+XMLParserErrorHandler::
 warning(const SAXParseException &ex)
 {
   char *msg = XMLString::transcode(ex.getMessage());
-  this->error(msg, ex.getLineNumber(), ex.getColumnNumber());
+  AssertThrow (false, ExcXMLWarning (msg, ex.getLineNumber(), ex.getColumnNumber()));
   XMLString::release(&msg);
 }
 
 
 
 void
-XMLFileParserHandler::
+XMLParserErrorHandler::
 error(const SAXParseException &ex)
 {
   char *msg = XMLString::transcode(ex.getMessage());
-  this->error(msg, ex.getLineNumber(), ex.getColumnNumber());
+  AssertThrow (false, ExcXMLError (msg, ex.getLineNumber(), ex.getColumnNumber()));
   XMLString::release(&msg);
 }
 
 
 
 void
-XMLFileParserHandler::
+XMLParserErrorHandler::
 fatalError(const SAXParseException &ex)
 {
   char *msg = XMLString::transcode(ex.getMessage());
-  this->error(msg, ex.getLineNumber(), ex.getColumnNumber());
+  AssertThrow (false, ExcXMLError (msg, ex.getLineNumber(), ex.getColumnNumber()));
   XMLString::release(&msg);
 }
 
 
 
 void
-XMLFileParserHandler::
+XMLParserErrorHandler::
 resetErrors()
 {}
-
-
-
-void
-XMLFileParserHandler::
-error(const bool cond,
-      const string &msg,
-      const int &line_number,
-      const int &column_number) const
-{
-//  IsoldeAssert(logs_.size() == file_paths_.size(),
-//               ExcDimensionMismatch(logs_.size(), file_paths_.size()));
-
-  const string line_str = line_number == -1 ?
-                          "unknown" : to_string(line_number);
-  const string col_str = column_number == -1 ?
-                         "unknown" : to_string(column_number);
-
-//  auto logs_it = logs_.crbegin();
-//  auto files_it = file_paths_.crbegin();
-//  auto files_end = file_paths_.crend();
-//
-//  string error_msg =
-//    "An error occurred parsing file: \"" + *files_it +
-//    "\" at line " + line_str  + " column " + col_str + ":\n\t" +
-//    (*logs_it)->get_tree_as_string("In XML tree tag ", ":\n\n");
-//
-//  ++files_it;
-//  ++logs_it;
-//
-//  for (; files_it != files_end; ++files_it, ++logs_it)
-//    error_msg +=
-//      "Called from file: \"" + *files_it + ":\n\t" +
-//      (*logs_it)->get_tree_as_string("In XML tree tag ", ":\n\n");
-//
-//  if (!cond)
-//    log_manager_->throw_error(error_msg + "\t" + msg);
-
-}
-
-
-
-
-void
-XMLFileParserHandler::
-error(const string &msg) const
-{
-  this->error(false, msg);
-}
-
-
-
-void
-XMLFileParserHandler::
-error(const string &msg,
-      const int &line_number,
-      const int &column_number) const
-{
-  this->error(false, msg, line_number, column_number);
-}
-
-
-
-void
-XMLFileParserHandler::
-add_id(const Index &id)
-{
-//  const auto &log = logs_.back();
-//  log->add_id(id);
-}
-
-
-
-void
-XMLFileParserHandler::
-add_tag_name(const string &tag_name)
-{
-//  const auto &log = logs_.back();
-//  log->add_tag_name(tag_name);
-}
-
-
-
-
-void
-XMLFileParserHandler::
-pop_tag_name()
-{
-//  const auto &log = logs_.back();
-//  log->pop_tag_name();
-}
-
-
-
-void
-XMLFileParserHandler::
-add_file(const string &file_path, const string &tag_name)
-{
-//  IsoldeAssert(logs_.size() == file_paths_.size(),
-//               ExcDimensionMismatch(logs_.size(), file_paths_.size()));
-  file_paths_.push_back(file_path);
-//  logs_.push_back(InputTreeLog::create(tag_name));
-}
-
-
-
-void
-XMLFileParserHandler::
-pop_file()
-{
-//  IsoldeAssert(logs_.size() == file_paths_.size(),
-//               ExcDimensionMismatch(logs_.size(), file_paths_.size()));
-  file_paths_.pop_back();
-//  logs_.pop_back();
-}
-
-
-
-void
-XMLFileParserHandler::
-print_info(LogStream &out) const
-{
-//  IsoldeAssert(logs_.size() == file_paths_.size(),
-//               ExcDimensionMismatch(logs_.size(), file_paths_.size()));
-
-//  Index level_id = 0;
-//  auto logs_it = logs_.cbegin();
-//  auto files_it = file_paths_.cbegin();
-//  auto files_end = file_paths_.cend();
-//  for (; files_it != files_end; ++files_it, ++logs_it, ++level_id)
-//  {
-//    out.begin_item("Level " + to_string(level_id));
-//    out << "Input file path:  " << *files_it << endl;
-//    (*logs_it)->print_info(out);
-//    out.end_item();
-//  }
-}
 
 
 IGA_NAMESPACE_CLOSE
